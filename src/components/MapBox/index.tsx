e4cff5da45eb5aa12ed613af9f9b5cfd72ea21da
SHO <admin@indietree.dev>
2022-10-15 21:41:10 +0900

    Added: Mapbox constants

--- src/components/MapBox/index.tsx
+++ src/components/MapBox/index.tsx
@@ -2,23 +2,23 @@ import { useEffect, useState, useRef } from 'react'
 
 // Lib
 import mapboxgl from 'mapbox-gl'
-
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_PUBLIC_TOKEN
+
+// Constants
+import initialValues from './constants'
 
 const Map = () => {
   const mapContainer = useRef(null)
   const map = useRef(null)
-  const [lng, setLng] = useState(-70.9)
-  const [lat, setLat] = useState(42.35)
-  const [zoom, setZoom] = useState(9)
+  const [mapBoxState, setMapBoxState] = useState(initialValues.mapbox)
 
   useEffect(() => {
     if (map.current) return // initialize map only once
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom,
+      center: [mapBoxState.lng, mapBoxState.lat],
+      zoom: mapBoxState.zoom,
     })
   })
 
