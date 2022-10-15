58096a0b0aee374968de9b6dd4522e3e999290a2
SHO <admin@indietree.dev>
2022-10-16 05:21:16 +0900

    Added: SVG Loader for storybook webpack config

--- .storybook/main.js
+++ .storybook/main.js
@@ -22,6 +22,26 @@ module.exports = {
       '@/components': path.resolve(__dirname, '../src/components'),
     }
 
+    config.module.rules = config.module.rules.map((rule) => {
+      if (
+        String(rule.test) ===
+        String(/\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/)
+      ) {
+        return {
+          ...rule,
+          test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/,
+        }
+      }
+
+      return rule
+    })
+
+    // use svgr for svg files
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack', 'url-loader'],
+    })
+
     return config
   },
 }
