8d0055ca046948f9c85b1a66d0aeeea12ca898c1
SHO <admin@indietree.dev>
2022-10-16 22:09:47 +0900

    Removed: unused code in webpackFinal

--- .storybook/main.js
+++ .storybook/main.js
@@ -18,26 +18,6 @@ module.exports = {
   },
   staticDirs: ['../public'],
   webpackFinal: async (config, { configType }) => {
-    // config.resolve.alias = {
-    //   ...config.resolve.alias,
-    //   '@/components': path.resolve(__dirname, '../src/components'),
-    // }
-
-    // config.module.rules = config.module.rules.map((rule) => {
-    //   if (
-    //     String(rule.test) ===
-    //     String(/\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/)
-    //   ) {
-    //     return {
-    //       ...rule,
-    //       test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/,
-    //     }
-    //   }
-
-    //   return rule
-    // })
-
-    // return config
     config.resolve.alias = {
       ...config.resolve.alias,
       '@/components': path.resolve(__dirname, '../src/components'),
