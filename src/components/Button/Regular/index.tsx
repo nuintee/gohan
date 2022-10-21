3ebdb499b508786985a2ff9a216ec2aa7b39f690
SHO <admin@indietree.dev>
2022-10-21 22:51:00 +0900

    Added: active bg color for danger button

--- src/components/Button/Regular/index.tsx
+++ src/components/Button/Regular/index.tsx
@@ -12,7 +12,7 @@ const Regular = (props: Props) => {
 
   const theme = {
     className: danger
-      ? 'ring-gh-red ring-1 text-gh-red bg-transparent'
+      ? 'ring-gh-red ring-1 text-gh-red bg-opacity-0 bg-gh-red active:bg-opacity-100 active:text-white'
       : outline &&
         'ring-gh-l-gray ring-1 text-gh-l-gray bg-opacity-0 bg-gh-l-gray active:bg-gray-200 active:bg-opacity-90',
     icon: danger ? 'gh-red' : outline ? 'gh-l-gray' : 'gh-white',
