a260a5073009b7a19db66e54d930c1c91a56006c
SHO <admin@indietree.dev>
2022-10-22 01:06:54 +0900

    Removed: Input group and changed Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,23 +1,36 @@
-const Input = () => {
-  return (
-    <div className='w-full flex bg-gh-white rounded-md overflow-hidden focus-within:ring-2 focus-within:ring-gh-l-gray'>
-      <input
-        type={'text'}
-        className='flex-1 outline-none px-2 py-1 bg-transparent'
-        placeholder='Text'
-      />
-      <button className='text-sm p-2 text-gh-gray outline-none active:text-black'>Action</button>
-    </div>
-  )
+type Props = {
+  label?: string
+  type: React.HTMLInputTypeAttribute
+  action?: {
+    label: string
+    onClick: React.MouseEventHandler<HTMLButtonElement>
+  }
+  placeholder?: string
 }
 
-const InputGroup = () => {
+const Input = (props: Props) => {
+  const { label, type, placeholder, action, ...rest } = props
   return (
     <div className='flex flex-col gap-1'>
-      <label className='text-gh-gray'>Action</label>
-      <Input />
+      {label && <label className='text-gh-gray'>{label}</label>}
+      <div className='w-full flex bg-gh-white rounded-md overflow-hidden focus-within:ring-2 focus-within:ring-gh-l-gray'>
+        <input
+          type={type || 'text'}
+          className='flex-1 outline-none px-2 py-1 bg-transparent'
+          placeholder={placeholder}
+          {...rest}
+        />
+        {action && (
+          <button
+            className='text-sm p-2 text-gh-gray outline-none active:text-black'
+            onClick={action.onClick}
+          >
+            {action.label}
+          </button>
+        )}
+      </div>
     </div>
   )
 }
 
-export { Input, InputGroup }
+export default Input
