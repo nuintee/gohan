6e74972fbcc3d495fd1bf49c0d7641c1b55fb724
SHO <admin@indietree.dev>
2022-11-01 04:41:39 +0900

    Added: disabled world copies

--- src/components/MapBox/index.tsx
+++ src/components/MapBox/index.tsx
@@ -12,13 +12,66 @@ const mapboxAccessToken = process.env.NEXT_PUBLIC_MAPBOX_PUBLIC_TOKEN
 
 const MapBox = (props) => {
   const mapRef = useRef(null)
-  const [layers, setLayers] = useState([])
-  const [geoData, setGeoData] = useState({})
+  const [sources, setSources] = useState([
+    {
+      id: 'route',
+      type: 'Feature',
+      geometry: {
+        type: 'Polygon',
+        coordinates: [
+          [
+            [100, 40],
+            [-66.96466, 44.8097],
+          ],
+        ],
+      },
+      layers: [
+        {
+          id: 'start',
+          type: 'circle',
+          source: 'route',
+          paint: {
+            'circle-color': '#4E3FC8',
+          },
+        },
+      ],
+    },
+    {
+      id: 'path',
+      type: 'Feature',
+      geometry: {
+        type: 'MultiLineString',
+        coordinates: [
+          [
+            [100, 40],
+            [100, 50],
+            [100, 60],
+            [-66.96466, 44.8097],
+          ],
+        ],
+      },
+      layers: [
+        {
+          id: 'p',
+          type: 'line',
+          source: 'path',
+          paint: {
+            'line-color': '#15cc09',
+            'line-width': 2,
+          },
+        },
+      ],
+    },
+  ])
   const { manageToast } = useToast()
+
+  const start = [100, 40]
 
   const onLoad = (e) => {}
 
-  const onClick = (e) => {}
+  const onClick = (e) => {
+    const coords = Object.keys(e.lngLat).map((key) => e.lngLat[key])
+  }
 
   return (
     <Map
@@ -33,6 +86,7 @@ const MapBox = (props) => {
       ref={mapRef}
       onClick={onClick}
       onLoad={onLoad}
+      renderWorldCopies={false}
     >
       <GeolocateControl
         trackUserLocation={true}
@@ -42,17 +96,22 @@ const MapBox = (props) => {
       />
       <Marker longitude={100} latitude={40}>
         <div className='relative'>
-          <div className='w-6 h-6 bg-gh-orange bg-opacity-75 rounded-full top-0 left-0 animate-ping'>
-            {/* BG */}
-          </div>
+          <div className='w-6 h-6 bg-gh-orange bg-opacity-75 rounded-full top-0 left-0 animate-ping'></div>
           <span className='h-4 w-4 bg-gh-orange rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 shadow-md'></span>
         </div>
       </Marker>
-      <Source id='route' type='geojson' data={geoData}>
+      {/* <Source id='route' type='geojson' data={geoData}>
         {layers.map((layer) => (
           <Layer {...layer} />
         ))}
-      </Source>
+      </Source> */}
+      {sources?.map((source) => (
+        <Source id={source.id} type='geojson' data={source}>
+          {source.layers.map((layer) => (
+            <Layer {...layer} />
+          ))}
+        </Source>
+      ))}
     </Map>
   )
 }
