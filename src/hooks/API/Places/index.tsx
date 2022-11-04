807a78d2772af1e2633c6b07344b5ef057d5bf56
SHO <admin@indietree.dev>
2022-11-05 07:02:28 +0900

    Added: usePlaces hook

--- src/hooks/API/Places/index.tsx
+++ src/hooks/API/Places/index.tsx
@@ -1,3 +1,25 @@
-const usePlaces = (coords) => {}
+import { Coords } from '@/types/GeoLocation/index.types'
+
+// Creds
+const gcpKey = process.env.NEXT_PUBLIC_GCP_API_KEY
+
+const usePlaces = (coords: Coords) => {
+  const is_devmode = process.env.NODE_ENV === 'development'
+
+  const get = async () => {
+    try {
+      const url = is_devmode
+        ? `/api/place?location=${coords.lat},${coords.lng}&radius=500`
+        : `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${coords.lat},${coords.lng}&radius=500&types=food&name=harbour&key=${gcpKey}`
+      const query = await fetch(url)
+      const json = await query.json()
+      return json
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return { get }
+}
 
 export default usePlaces
