6d498bc80a7414bde49fd4e0cdf3f897fd2db183
SHO <admin@indietree.dev>
2022-11-17 07:29:25 +0900

    Added: default size to Logo

--- src/icons/Logo/index.tsx
+++ src/icons/Logo/index.tsx
@@ -3,8 +3,8 @@ import { Props } from '../index.types'
 const Logo = (props: Props) => {
   return (
     <svg
-      width={props.width}
-      height={props.height}
+      width={props.width || 35}
+      height={props.height || 35}
       viewBox='0 0 114 114'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
