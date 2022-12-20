f623e13bcb7593d25e2290aadca9f87141bdbb08
SHO <admin@indietree.dev>
2022-12-21 03:17:33 +0900

    Added: distanceFormatter

--- src/components/Restaurant/Label/index.tsx
+++ src/components/Restaurant/Label/index.tsx
@@ -2,8 +2,16 @@
 import Route from './icons/Route'
 
 type Props = {
-  distance: number
+  distance: number // km
   extraClassName: string
+}
+
+const distanceFormatter = (distance: number) => {
+  if (distance >= 1) {
+    return distance + 'km'
+  } else if (distance < 1) {
+    return Math.round(distance * 1000) + 'm'
+  }
 }
 
 const Label = (props: Props) => {
@@ -13,7 +21,7 @@ const Label = (props: Props) => {
       className={`flex gap-2 items-center bg-gh-gray w-fit px-2 py-1 rounded-md text-white ${extraClassName}`}
     >
       <Route height={12} width={12} />
-      {distance}km
+      {distanceFormatter(distance)}
     </span>
   )
 }
