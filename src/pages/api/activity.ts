78a47f06b133b1a5e1d17420380523807038e712
nuintee <nuinteedev@gmail.com>
2023-01-05 05:57:46 +0900

    add: API route with get all and post dummy data

---
+++ src/pages/api/activity.ts
@@ -0,0 +1,26 @@
+import prisma from '@/lib/prisma'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { randomUUID } from 'crypto'
+
+type Data = {
+  name: string
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  switch (req.method) {
+    case 'POST':
+      const posted = await prisma.activity.create({
+        data: {
+          user_id: '1',
+          place_id: 'de',
+          is_liked: true,
+          discovered_at: new Date().toISOString(),
+        },
+      })
+      res.json(posted)
+      break
+    case 'GET':
+      const result = await prisma.activity.findMany()
+      res.json(result)
+  }
+}
