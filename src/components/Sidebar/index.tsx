229cb501047d3522ddfab6574983bb11f8a69ab0
nuintee <nuinteedev@gmail.com>
2023-01-04 23:13:30 +0900

    perf: ⚡️ hidden setActivityData
    commented out the init function inside of a useEffect in Sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -58,7 +58,7 @@ const Sidebar = (props: Props) => {
 
   useEffect(() => {
     const init = async () => {
-      setActivityData(activities)
+      // setActivityData(activities)
     }
 
     init()
