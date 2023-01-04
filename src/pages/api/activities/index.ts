ebd9de73ddc23eeffab0cfe84e073994705e1a63
nuintee <nuinteedev@gmail.com>
2023-01-05 07:05:10 +0900

    add: ➕ index API route for activities

---
+++ src/pages/api/activities/index.ts
@@ -0,0 +1,30 @@
+import prisma from '@/lib/prisma'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  name: string
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const { user_id } = req.query
+  switch (req.method) {
+    case 'POST':
+      const posted = await prisma.activity.create({
+        data: {
+          user_id,
+          place_id: 'de',
+          is_liked: true,
+          discovered_at: new Date().toISOString(),
+        },
+      })
+      res.json(posted)
+      break
+    case 'GET':
+      const result = await prisma.activity.findMany({
+        where: {
+          user_id: '1',
+        },
+      })
+      res.json(result)
+  }
+}
