fa9a8cfb61cdf1118ce7a56cbbea7a223dcfa19d
nuintee <nuinteedev@gmail.com>
2023-01-07 08:06:19 +0900

    fix: activityTable add acontroller

--- src/pages/api/v1/activities/index.ts
+++ src/pages/api/v1/activities/index.ts
@@ -1,15 +1,20 @@
 import { handleRequest, activityTable } from '@/hooks/API/prisma'
 import prisma from '@/lib/prisma'
+import { randomUUID } from 'crypto'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
 }
 
+// GET | POST | DELETE
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   switch (req.method) {
     case 'GET':
       await handleRequest(() => activityTable.getAll(req.query), res)
+      break
+    case 'POST':
+      await handleRequest(() => activityTable.add(req.body), res)
       break
     default:
       break
