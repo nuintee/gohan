c7305d0c1e57fa66a7c6764461809dfeabdb08e8
nuintee <nuinteedev@gmail.com>
2023-01-07 04:08:59 +0900

    add: user/:userId controllers

--- src/pages/api/v1/user/[userId].ts
+++ src/pages/api/v1/user/[userId].ts
@@ -1,5 +1,6 @@
 import prisma from '@/lib/prisma'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { handle_request, userTable } from '@/hooks/API/prisma'
 
 type Data = {
   name: string
@@ -7,8 +8,46 @@ type Data = {
 
 // GET | PATCH | DELETE
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json({
-    status: 'alive',
-    code: 200,
-  })
+  const { userId } = req.query
+  switch (req.method) {
+    case 'GET':
+      await handle_request(
+        () =>
+          prisma.user.findUnique({
+            where: {
+              id: userId,
+            },
+          }),
+        res,
+      )
+      break
+    case 'PATCH':
+      await handle_request(
+        () =>
+          prisma.user.update({
+            where: {
+              id: userId,
+            },
+            data: {
+              email: req.body?.email,
+              username: req.body?.username,
+            },
+          }),
+        res,
+      )
+      break
+    case 'DELETE':
+      await handle_request(
+        () =>
+          prisma.user.delete({
+            where: {
+              id: userId,
+            },
+          }),
+        res,
+      )
+      break
+    default:
+      break
+  }
 }
