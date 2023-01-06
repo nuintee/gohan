0792e0d1afd2cfc09c71232411b68bf23bc8333e
nuintee <nuinteedev@gmail.com>
2023-01-07 06:57:01 +0900

    change: renamed and adjusted activityTable helper function

---
+++ src/hooks/API/prisma/activity/index.ts
@@ -0,0 +1,63 @@
+import prisma from '@/lib/prisma'
+import { randomUUID } from 'crypto'
+import { resultFilter } from '..'
+
+type UserKey = {
+  id: string | string[] | undefined
+}
+
+type Data = {
+  place_id: string
+  is_liked: boolean
+}
+
+type ListFilter = {
+  limit?: number
+  offset?: number
+}
+
+type MutateProps = UserKey & Data
+
+const activityTable = {
+  get: async (props: UserKey) => {
+    const fetchedActivity = await prisma.activity.findUniqueOrThrow({
+      where: {
+        id: props.id,
+      },
+    })
+    return fetchedActivity
+  },
+  getAll: async (props: ListFilter) => {
+    const fetchedActivities = await prisma.activity.findMany(resultFilter(props))
+    return fetchedActivities
+  },
+  add: async (props: Data) => {
+    const id = randomUUID()
+    const addedActivity = await prisma.activity.create({
+      data: { ...props, id, discovered_at: new Date().toISOString() },
+    })
+    return addedActivity
+  },
+  patch: async (props: MutateProps) => {
+    const updatedUser = await prisma.activity.update({
+      where: {
+        id: props.id,
+      },
+      data: {
+        is_liked: props?.is_liked,
+        place_id: props?.place_id,
+      },
+    })
+    return updatedUser
+  },
+  delete: async (props: UserKey) => {
+    const deletedUsers = await prisma.activity.delete({
+      where: {
+        id: props.id,
+      },
+    })
+    return deletedUsers
+  },
+}
+
+export default activityTable
