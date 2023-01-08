ebd661b9915ff59f42c537d36cd90f781a9caeb0
nuintee <nuinteedev@gmail.com>
2023-01-09 04:28:27 +0900

    add: getRestaurant base function in Provider RefactorGEO#36

--- src/context/Restaurants/index.tsx
+++ src/context/Restaurants/index.tsx
@@ -6,16 +6,23 @@ type Props = {
   children: JSX.Element
 }
 
+type RestaurantGetOptions = {}
+
 const RestaurantsContext = createContext({})
 
 const RestaurantsProvider = (props: Props) => {
   const { children } = props
 
+  const [restaurantsList, setRestaurantsList] = useState<ResultsEntity[] | []>([])
   const [restaurant, setRestaurant] = useState<ResultsEntity | {}>({})
+
+  const getRestaurant = (options: RestaurantGetOptions) => {}
 
   const value = {
     restaurant,
     setRestaurant,
+    restaurantsList,
+    setRestaurantsList,
   }
 
   return <RestaurantsContext.Provider value={value}>{children}</RestaurantsContext.Provider>
