d29017f70fd820ba299eab9c5f560d28891b0253
nuintee <nuinteedev@gmail.com>
2023-01-09 03:47:49 +0900

    add: restaurants provider RefactorGEO#36

---
+++ src/context/Restaurants/index.ts
@@ -0,0 +1,32 @@
+import React, { useState, createContext, ReactNode } from 'react'
+
+const initialValues = {
+  activity: {
+    isOpen: false,
+  },
+}
+
+type Props = {
+  children: JSX.Element
+}
+
+type InitialValues = typeof initialValues
+
+type SidebarTypes = keyof InitialValues
+
+const RestaurantsContext = createContext({})
+
+const RestaurantsProvider = (props: Props) => {
+  const { children } = props
+
+  const value = {}
+
+  return <RestaurantsContext.Provider value={value}>{children}</RestaurantsContext.Provider>
+}
+
+const Sidebar = {
+  RestaurantsContext,
+  RestaurantsProvider,
+}
+
+export default Sidebar
