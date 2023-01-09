0b6d2ff2930649493de79c7aa36999140bf8a442
nuintee <nuinteedev@gmail.com>
2023-01-10 05:15:38 +0900

    update: coords type feature/destination#40

--- src/components/MapBox/components/DestinationMarker/index.tsx
+++ src/components/MapBox/components/DestinationMarker/index.tsx
@@ -1,10 +1,11 @@
+import { Coords } from '@/constants/coords'
 import { Marker } from 'react-map-gl'
 
-const DestinationMarker = (props) => {
+const DestinationMarker = (props: { coords: Coords }) => {
   const { coords } = props
 
   return (
-    <Marker longitude={coords?.longitude} latitude={coords?.latitude}>
+    <Marker longitude={coords.longitude} latitude={coords.latitude}>
       <div className='relative'>
         <div className='w-8 h-8 bg-gh-orange bg-opacity-75 rounded-full top-0 left-0 animate-ping'></div>
         <span className='h-6 w-6 bg-gh-orange rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 shadow-md'></span>
