b2239023f2590d96207d7fac7efa080f435fe55e
nuintee <nuinteedev@gmail.com>
2023-01-10 03:44:06 +0900

    add: getRoute function RefactorGEO#36

--- src/hooks/API/restaurant/index.tsx
+++ src/hooks/API/restaurant/index.tsx
@@ -23,7 +23,26 @@ const useRestaurantSearch = () => {
     setRestaurant({})
   }
 
-  return { getRestaurant, clearRestaurant }
+  const getRoute = async ({ profileType, start, end }) => {
+    try {
+      const base_coordinates = encodeURIComponent(`${start};${end}`)
+      const profile = `mapbox/${profileType || 'walking'}`
+      const query = await fetch(
+        `https://api.mapbox.com/directions/v5/${profile}/${base_coordinates}?alternatives=true&geometries=geojson&language=en&overview=simplified&access_token=${mapboxAccessToken}`,
+      )
+      const json = await query.json()
+      const routes = json?.routes
+      const waypoints = json?.waypoints
+      const coordinates = routes?.map((route) => route.geometry.coordinates)
+      const endpoint = waypoints.map((waypoint) => waypoint.location)
+      // addSource
+      console.log(endpoint)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return { getRestaurant, clearRestaurant, getRoute }
 }
 
 export default useRestaurantSearch
