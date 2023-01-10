5a94314904e3d060948cf44f04b2dde5a06a278e
nuintee <nuinteedev@gmail.com>
2023-01-11 01:24:12 +0900

    add: dummy data on data empty Restaurants#45

--- src/components/Restaurant/index.tsx
+++ src/components/Restaurant/index.tsx
@@ -21,7 +21,30 @@ const Restaurant = (props: RestaurantProps) => {
   const { mode, data, is_liked } = props
   const { calculateDistance, currentPosition } = useGPS()
 
-  if (!data) return <></>
+  // Dummy
+  if (!data)
+    return (
+      <div
+        className='flex bg-white p-2 rounded-md justify-between items-center gap-4 h-28 w-fill cursor-pointer active:bg-gray-50 active:scale-95'
+        onClick={() => {}}
+      >
+        <img
+          src={
+            'https://images.unsplash.com/photo-1508424757105-b6d5ad9329d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80'
+          }
+          className='max-h-full max-w-full h-auto w-auto aspect-square object-cover rounded-md'
+          alt={'image'}
+        />
+
+        <div className='flex flex-1 gap-4 items-start'>
+          <div className='flex flex-col gap-2'>
+            <Texts main={'NAME'} sub={''} size='small' />
+            <Label distance={'300m'} />
+          </div>
+          <Like onClick={() => {}} state={is_liked ? 'LIKED' : 'UNLIKED'} />
+        </div>
+      </div>
+    )
 
   const { distance } = calculateDistance(
     _formatObjectCoords(data.geometry.location),
@@ -38,8 +61,8 @@ const Restaurant = (props: RestaurantProps) => {
       >
         <img
           src={
-            photos?.length
-              ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${photos[0]?.photo_reference}&key=${process.env.NEXT_PUBLIC_GCP_API_KEY}`
+            data?.photos?.length
+              ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${data?.photos[0]?.photo_reference}&key=${process.env.NEXT_PUBLIC_GCP_API_KEY}`
               : 'https://images.unsplash.com/photo-1508424757105-b6d5ad9329d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80'
           }
           className='max-h-full max-w-full h-auto w-auto aspect-square object-cover rounded-md'
