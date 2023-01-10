307dc9005d9936c7e09e136f94b9c4a1181b902e
nuintee <nuinteedev@gmail.com>
2023-01-10 10:18:56 +0900

    remove: unused import at MapBox component feature/destination#43

--- src/components/MapBox/index.tsx
+++ src/components/MapBox/index.tsx
@@ -1,11 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { CurrentPostion } from '@/icons'
+import React, { useEffect } from 'react'
 import { type FC } from 'react'
-import Map, { GeolocateControl, Popup, Marker, Source, Layer } from 'react-map-gl'
+import Map, { Source, Layer } from 'react-map-gl'
 import { useMapBox } from '@/hooks/context'
 import useGPS from '@/hooks/context/GPS'
 import { CurrentLocationMarker, DestinationMarker } from './components'
-import { DEV_GEO_JSON, DEV_LAYER, DEV_ROUTES, DEV_TARGET_COORDS } from './data'
 
 // Config
 const mapboxAccessToken = process.env.NEXT_PUBLIC_MAPBOX_PUBLIC_TOKEN
