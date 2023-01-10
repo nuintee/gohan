0f8448e01ff9dca484d52f4972428bb0a1cb2e5c
nuintee <nuinteedev@gmail.com>
2023-01-10 09:18:02 +0900

    add: conditional Source render feature/destination#40

--- src/components/MapBox/index.tsx
+++ src/components/MapBox/index.tsx
@@ -6,10 +6,6 @@ import { useMapBox } from '@/hooks/context'
 import useGPS from '@/hooks/context/GPS'
 import { CurrentLocationMarker, DestinationMarker } from './components'
 import { DEV_GEO_JSON, DEV_LAYER, DEV_ROUTES, DEV_TARGET_COORDS } from './data'
-import useRestaurantSearch from '@/hooks/API/restaurant'
-
-// utils
-import { createSource } from './utils'
 
 // Config
 const mapboxAccessToken = process.env.NEXT_PUBLIC_MAPBOX_PUBLIC_TOKEN
@@ -28,10 +24,6 @@ const MapBox: FC<MapBoxProps> = (props) => {
     clearRoute,
   } = useMapBox()
   const { currentPosition } = useGPS()
-
-  useEffect(() => {
-    console.log({ directions, isNavigating })
-  }, [directions, isNavigating])
 
   const onClick = async (e) => {
     const { lngLat } = e
@@ -74,39 +66,11 @@ const MapBox: FC<MapBoxProps> = (props) => {
         <CurrentLocationMarker coords={currentPosition} />
         <DestinationMarker coords={DEV_TARGET_COORDS} />
 
-        {/* {isNavigating && (
+        {isNavigating && (
           <Source data={directions.source} type='geojson'>
             <Layer {...directions.layer} />
           </Source>
-        )} */}
-
-        {/* {isLocationReady && (
-          <Marker longitude={geoState.lng} latitude={geoState.lat}>
-            <div className='relative'>
-              <div className='w-8 h-8 bg-white bg-opacity-75 rounded-full top-0 left-0 animate-ping'></div>
-              <span className='h-6 w-6 bg-white rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 shadow-md flex items-center justify-center'>
-                <CurrentPostion width={12} height={12} />
-              </span>
-            </div>
-          </Marker>
         )}
-        {destination.length ? (
-          <Marker longitude={destination[0]} latitude={destination[1]}>
-            <div className='relative'>
-              <div className='w-8 h-8 bg-gh-orange bg-opacity-75 rounded-full top-0 left-0 animate-ping'></div>
-              <span className='h-6 w-6 bg-gh-orange rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 shadow-md'></span>
-            </div>
-          </Marker>
-        ) : (
-          <></>
-        )}
-        {sources?.map((source) => (
-          <Source id={source.id} type='geojson' data={source}>
-            {source.layers.map((layer) => (
-              <Layer {...layer} />
-            ))}
-          </Source>
-        ))} */}
       </Map>
     </div>
   )
