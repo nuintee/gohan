b0bd4cae48579bac89ddf1008c8add72ffba6fd7
nuintee <nuinteedev@gmail.com>
2023-01-17 22:54:52 +0900

    add: getRestaurant API Restaurants#45

--- src/pages/api/place.ts
+++ src/pages/api/place.ts
@@ -1,16 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Coords } from '@/constants/coords'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-import mapData from '@/data/places/index.json'
+const mapboxAccessToken = process.env.NEXT_PUBLIC_MAPBOX_PUBLIC_TOKEN
 
 type Data = {
   name: string
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  const onlyOpenNow = mapData.results.filter((map, index) => map.opening_hours?.open_now)
-  const randomIndex = Math.floor(Math.random() * onlyOpenNow.length)
-  const randomOne = onlyOpenNow[randomIndex]
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const { profileType, start, end } = req.query
 
-  return res.status(200).json(randomOne)
+  const base_coordinates = encodeURIComponent(`${start};${end}`)
+  const profile = `mapbox/${profileType || 'walking'}`
+  const baseURL = `https://api.mapbox.com/directions/v5/${profile}/${base_coordinates}?alternatives=true&continue_straight=true&geometries=geojson&language=en&overview=simplified&steps=true&access_token=${mapboxAccessToken}`
+
+  try {
+    const query = await fetch(baseURL)
+    const data = await query.json()
+    const coordinates = data?.routes[0].geometry.coordinates
+    res.status(200).json({ data, coordinates })
+  } catch (error) {
+    res.status(500).json(error)
+  }
 }
