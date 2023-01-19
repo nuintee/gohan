4f485658eed2471bcd1c6d9b79c13ffce9d1b74a
nuintee <nuinteedev@gmail.com>
2023-01-20 02:57:02 +0900

    add: activities, mapbox, restaurants directory under features refactor/directory-structure#52

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,7 @@
-const BadComponent = () => {
-  const chance = Math.random() > 0.5
-
-  if (chance) throw new Error('This is bad!')
-
-  return <>Bad</>
-}
-
 const Index = () => {
   return (
     <div>
       <p>Index</p>
-      <BadComponent />
     </div>
   )
 }
