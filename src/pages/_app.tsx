c7ecc1e32a5ec0a980e6a336a90fe93bf53c06d1
nuintee <nuinteedev@gmail.com>
2023-01-20 02:52:10 +0900

    add: ErrorBoundary refactor/directory-structure#52

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,15 @@ import '../styles/globals.css'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import ErrorBoundary from '@/components/fallback/ErrorBoundary'
 
 function App({ Component, pageProps }: AppProps<{ session: Session }>) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ErrorBoundary>
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ErrorBoundary>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
+const BadComponent = () => {
+  const chance = Math.random() > 0.5
+
+  if (chance) throw new Error('This is bad!')
+
+  return <>Bad</>
+}
+
 const Index = () => {
-  return <div>Index</div>
+  return (
+    <div>
+      <p>Index</p>
+      <BadComponent />
+    </div>
+  )
 }
 
 export default Index
