af7da35e008f4c12994d9f0bf2b1723d90752f0e
nuintee <nuinteedev@gmail.com>
2023-01-20 19:41:59 +0900

    update: ActivityPanel under components refactor/directory-structure#52

---
+++ src/components/ui/Header/index.tsx
@@ -0,0 +1,24 @@
+import { MouseEventHandler } from 'react'
+
+// Icon
+import { Close } from '@/components/icons'
+
+type Props = {
+  title?: string
+  onClose?: MouseEventHandler<HTMLButtonElement>
+}
+
+const Header = (props: Props) => {
+  const { title, onClose } = props
+
+  return (
+    <header className='p-4 flex gap-2 items-center justify-between border-gh-l-gray'>
+      <p className='font-bold'>{title || 'TITLE'}</p>
+      <button onClick={onClose}>
+        <Close fill='black' />
+      </button>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/features/activities/components/ActivityPanel.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react'
+
+// Components
+import Header from '@/components/ui/Header'
+
+// Constants
+const tabs = [
+  {
+    label: 'History',
+    id: 0,
+  },
+  {
+    label: 'Favorites',
+    id: 1,
+  },
+]
+
+const placeholder_data = Array.from(Array(100).keys()).map((v) => ({
+  state: ['LIKED', 'UNLIKED'][Math.floor(Math.random() * ['LIKED', 'UNLIKED'].length)],
+}))
+
+type Props = {
+  isOpen: boolean
+}
+
+const ActivityPanel = (props: Props) => {
+  const { isOpen } = props
+  const slideIn = isOpen ? '-transform-x-full' : 'translate-x-full'
+
+  return (
+    <div
+      className={`absolute top-0 right-0 h-screen bg-white flex flex-col min-w-[20rem] w-fit duration-700 ease-in-out rounded-tl-md rounded-bl-md z-[2] ${slideIn}`}
+    >
+      <Header title={'ActivityPanel'} onClose={() => {}} />
+      {/* <Tab tabs={tabs} selectedId={selectedId} onSelect={setTabs} />
+      <Renderer data={activityList} isLocked={status !== 'authenticated'} onLike={handleOnLike} /> */}
+    </div>
+  )
+}
+
+export default ActivityPanel
