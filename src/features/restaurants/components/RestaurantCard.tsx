fee57262a7f36d83055b4cf435ba36dfbf48022e
nuintee <nuinteedev@gmail.com>
2023-01-21 00:11:39 +0900

    add: Restaurant Layout refactor/directory-structure#52

--- src/features/restaurants/components/RestaurantCard.tsx
+++ src/features/restaurants/components/RestaurantCard.tsx
@@ -74,8 +74,60 @@ const Compact = (props: RestaurantProps<{ compact?: boolean }>) => {
   )
 }
 
+// 共通部分
+const Layout = (props: RestaurantProps<{ compact?: boolean }>) => {
+  const { compact, data } = props
+
+  const cardStyle = {
+    container: 'max-w-[20rem] rounded-md overflow-hidden bg-white relative',
+    img: `select-none max-h-52 w-full object-cover h-52`,
+    contents: 'p-4 flex flex-col gap-4',
+    infoContainer: 'flex flex-row-reverse gap-2',
+    actionsContainer: 'flex w-full gap-4',
+  }
+
+  const compactStyle = {
+    container:
+      'flex bg-white p-2 rounded-md justify-between items-center gap-4 h-28 w-fill cursor-pointer active:bg-gray-50 active:scale-95',
+    img: 'max-h-full max-w-full h-auto w-auto aspect-square object-cover rounded-md',
+    contents: 'flex flex-1 gap-4 items-start justify-between',
+    infoContainer: 'flex flex-col gap-2',
+    actionsContainer: '',
+  }
+
+  const theme = compact ? compactStyle : cardStyle
+
+  return (
+    <div className={theme.container}>
+      {!compact && (
+        <button className='absolute left-[1rem] top-[1rem] outline-none z-10' onClick={() => {}}>
+          <Close fill={CARD_CONFIG.CLOSE_COLOR} />
+        </button>
+      )}
+      <img className={theme.img} />
+      <div className={theme.contents}>
+        <div className={theme.infoContainer}>
+          <Texts
+            main={CARD_CONFIG.textsMain(data?.name)}
+            sub={CARD_CONFIG.textsSub(data?.types?.join('・'))}
+            size={CARD_CONFIG.textsSize(compact)}
+          />
+          <Label text={CARD_CONFIG.labelDistance()} icon={CARD_CONFIG.labelIcon} />
+        </div>
+        <div className={theme.actionsContainer}>
+          {!compact && <Button text={CARD_CONFIG.buttonText()} />}
+          <LikeButton onClick={() => {}} isLiked={Boolean(true)} isLocked={true} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
 const RestaurantCard = (props: RestaurantProps<{ compact?: boolean }>) => {
   const { compact } = props
+
+  return <Layout {...props} />
+
   if (compact) {
     return <Compact {...props} />
   } else {
