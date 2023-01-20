895d4708bb2d407a8146301fc989badd3fa7f753
nuintee <nuinteedev@gmail.com>
2023-01-21 01:30:10 +0900

    update: storybook config refactor/directory-structure#52
    layout fullscreen

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -11,4 +11,5 @@ export const parameters = {
   backgrounds: {
     default: 'dark',
   },
+  layout: 'fullscreen',
 }
--- src/features/mapbox/components/MapBox.tsx
+++ src/features/mapbox/components/MapBox.tsx
@@ -1,19 +1,21 @@
 import Map, { Source, Layer } from 'react-map-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
 // config
 import { MAPBOX_PUBLIC_TOKEN } from '@/config/env'
 
 const MapBox = ({}) => {
   return (
-    <div className='w-screen h-screen'>
-      <Map
-        mapboxAccessToken={MAPBOX_PUBLIC_TOKEN}
-        mapStyle='mapbox://styles/mapbox/streets-v11'
-        style={{ width: '100vw', height: '100vh' }}
-        renderWorldCopies={false}
-        pitchWithRotate={false}
-      ></Map>
-    </div>
+    // <div className='w-screen h-screen'>
+    //   <Map
+    //     mapboxAccessToken={MAPBOX_PUBLIC_TOKEN}
+    //     mapStyle='mapbox://styles/mapbox/streets-v11'
+    //     style={{ width: '100vw', height: '100vh' }}
+    //     renderWorldCopies={false}
+    //     pitchWithRotate={false}
+    //   ></Map>
+    // </div>
+    <div className='bg-red-200 h-full w-full'>1</div>
   )
 }
 
