9c789544cfd251b352b11f9b5b26c4242c196bba
nuintee <nuinteedev@gmail.com>
2023-01-22 08:04:10 +0900

    add: UserAuthModal Base components/user-modals#65
    NOT DONE YET: Input #66, Header #67 Components are required

---
+++ src/features/user/components/UserAuthModal.stories.tsx
@@ -0,0 +1,18 @@
+import React, { ComponentProps } from 'react'
+
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import UserAuthModal from './UserAuthModal'
+
+export default {
+  title: 'Features/Users/Modals/UserAuthModal',
+  component: UserAuthModal,
+} as ComponentMeta<typeof UserAuthModal>
+
+const Template: ComponentStory<typeof UserAuthModal> = (args) => {
+  return <UserAuthModal {...args} />
+}
+
+export const Default = Template.bind({})
+
+Default.args = {}
