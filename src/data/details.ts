363e78dd718702864f8b886a352ca218e9a51a97
nuintee <nuinteedev@gmail.com>
2023-01-22 00:34:58 +0900

    add: activities data refactor/directory-structure#52

--- src/data/details.ts
+++ src/data/details.ts
@@ -1,6 +1,6 @@
 import _places from './_places.json'
 
-export const _details = {
+export const details = {
   html_attributions: [],
   result: (place_id: string) => _places.results.find((place) => place.place_id === place_id),
   status: 'OK',
