9ae9f100d107af5ee925a5098fa1216cb7459e2b
nuintee <nuinteedev@gmail.com>
2023-01-22 08:38:04 +0900

    add: base layout of UserAuthModal components/user-modals#65

--- src/features/user/components/UserAuthModal.tsx
+++ src/features/user/components/UserAuthModal.tsx
@@ -1,39 +1,49 @@
+import { Header, Button, Input } from '@/components/ui'
 import ModalLayout from '@/layouts/ModalLayout'
 
-// const UserAuthModal = (props: any) => {
-//     return <p>Sigma</p>
-//   return (
-//     // <ModalLayout isOpen={true}>
-//     //   {/* <Header title='User' onClose={onClose} /> */}
-//     //   <>
-//     //     <main className='p-4 flex flex-col gap-4'>
-//     //       {/* {users.map((conf, index) => (
-//     //     //   <Input {...conf} label={conf.label} action={conf.action} key={index} />
-//     //     <input {conf.label}/>
-//     //     ))} */}
-//     //       <details>
-//     //         <summary className='text-gh-gray select-none cursor-pointer'>Advanced</summary>
-//     //         <div className='flex flex-col pt-4 gap-2'>
-//     //           {/* <Regular danger text='Delete account' /> */}
-//     //         </div>
-//     //       </details>
-//     //     </main>
-//     //     <hr></hr>
-//     //     <footer className='p-4 flex flex-col gap-2'>{/* <Regular text='Signout' /> */}</footer>
-//     //   </>
-//     // </ModalLayout>
-//   )
-// }
+{
+  /* <Layout isOpen={isOpen} zIndex={'z-[1]'}>
+        <section
+          className={`bg-white duration-700 rounded-md min-w-[20rem] z-[10000] ${
+            isOpen ? 'scale-100' : 'scale-0'
+          }`}
+        >
+          <Header title='Signup' onClose={onClose} />
+          <Tab tabs={tabs} selectedId={selecteTabId} onSelect={(id) => setSelectedTabId(id)} />
+          <main className='p-4 flex flex-col gap-4'>
+            {users.map((conf, index) => (
+              <Input {...conf} label={conf.label} action={conf.action} key={index} />
+            ))}
+          </main>
+          <hr></hr>
+          <footer className='p-4 flex flex-col gap-2'>
+            <Regular text='Signup' />
+          </footer>
+        </section>
+      </Layout> */
+}
+
 const UserAuthModal = () => {
   return (
     <ModalLayout isOpen={true}>
-      <section>
-        <details>
-          <summary className='text-gh-gray select-none cursor-pointer'>Advanced</summary>
-          <div className='flex flex-col pt-4 gap-2'>
-            {/* <Regular danger text='Delete account' /> */}
-          </div>
-        </details>
+      <section className='min-w-[20rem] bg-white flex flex-col'>
+        <Header title='Sign up' />
+        <hr></hr>
+        <main className='flex-1 p-4 flex flex-col gap-4'>
+          <Input label='Email' type={'email'} placeholder='ex: john@example.com' />
+          <Input
+            label='Password'
+            type={'password'}
+            action={{
+              label: 'Show',
+              onClick: () => {},
+            }}
+          />
+        </main>
+        <hr></hr>
+        <footer className='p-4 flex flex-col gap-2'>
+          <Button text='Sign up' />
+        </footer>
       </section>
     </ModalLayout>
   )
