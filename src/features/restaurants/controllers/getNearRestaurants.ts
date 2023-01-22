2713690dc858f52a462887ada008dbcc070304ca
nuintee <nuinteedev@gmail.com>
2023-01-23 08:26:16 +0900

    add: getRestaurants controllers/restaurants#76

--- src/features/restaurants/controllers/getNearRestaurants.ts
+++ src/features/restaurants/controllers/getNearRestaurants.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 // Env
 import { GCP_API_KEY } from '@/config/env'
 import axios from '@/libs/axios'
-import { PlacesAPI, ResultsEntity } from '../types'
+import { PlacesAPI } from '../types'
 
 const Schema = z.object({
   latitude: z
@@ -43,29 +43,7 @@ const getNearRestaurants = async (props: Props) => {
 
   if (!randomOne) return data
 
-  return data.results[0]
+  return data.results[Math.floor(Math.random() * data.results?.length)]
 }
 
 export default getNearRestaurants
-
-// export async function fetchNearRestaurants({
-//     latitude,
-//     longitude,
-//   }: {
-//     latitude?: string | null
-//     longitude?: string | null
-//   }) {
-//     if (!latitude || !longitude) throw new Error('invalid parameters')
-
-//     const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude}%2C${longitude}&radius=500&types=food&opennow=true&key=${GCP_KEY}`
-//     const data: PlacesAPI = await _handleFetch(url)
-//     return data
-//   }
-
-//   export async function fetchRestaurantDetail({ place_id }: { place_id?: string | null }) {
-//     if (!place_id) throw new Error('invalid parameters')
-
-//     const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&key=${GCP_KEY}`
-//     const data: DetailsAPI = await _handleFetch(url)
-//     return data
-//   }
