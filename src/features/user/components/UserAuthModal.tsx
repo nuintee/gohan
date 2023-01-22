984bca6b2c0e846397393d8da1be812b8092cabb
nuintee <nuinteedev@gmail.com>
2023-01-22 23:12:12 +0900

    add: UserAuthModal component components/user-modals#65

--- src/features/user/components/UserAuthModal.tsx
+++ src/features/user/components/UserAuthModal.tsx
@@ -7,7 +7,7 @@ import { z } from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 
-const tabs = ['Signin', 'Login']
+const tabs = ['Signup', 'Login']
 
 type Props = {
   isOpen: boolean
@@ -37,7 +37,7 @@ const UserAuthModal = (props: Props) => {
   return (
     <ModalLayout isOpen={isOpen}>
       <section className='min-w-[20rem] bg-white'>
-        <Header title='Auth' />
+        <Header title='Auth' onClose={onClose} />
         {tabs.map((tab, index) => (
           <button
             className={`border-b-2 h-full px-4 py-2 text-gh-gray box-border ${
@@ -50,17 +50,20 @@ const UserAuthModal = (props: Props) => {
             {tab}
           </button>
         ))}
+        <hr></hr>
         <form
-          onSubmit={handleSubmit((d) => console.log(d))}
+          onSubmit={handleSubmit((d) => onClickAction(d))}
           className='flex flex-col gap-4 p-4 bg-white'
         >
-          <Input
-            label='Username'
-            register={register}
-            registerName='username'
-            placeholder='ex: john0906'
-            errorMessage={errors.username?.message}
-          />
+          {tabs[tabination] === 'Signup' && (
+            <Input
+              label='Username'
+              register={register}
+              registerName='username'
+              placeholder='ex: john0906'
+              errorMessage={errors.username?.message}
+            />
+          )}
           <Input
             label='Email'
             register={register}
@@ -77,57 +80,11 @@ const UserAuthModal = (props: Props) => {
             required
           />
           <hr></hr>
-          <Button text='Signup' />
+          <Button text={tabs[tabination]} />
         </form>
       </section>
     </ModalLayout>
   )
 }
-
-// const UserAuthModal = (props: Props) => {
-//   const { isOpen } = props
-
-//   const [tabination, setTabination] = useState(0)
-
-//   return (
-//     <ModalLayout isOpen={isOpen}>
-//       <section className='min-w-[20rem] bg-white flex flex-col'>
-//         <Header title='Auth' />
-//         <div className='flex gap-4 items-center  border-b-[1px]' id='tab'>
-//           {tabs.map((tab, index) => (
-//             <button
-//               className={`border-b-2 h-full px-4 py-2 text-gh-gray box-border ${
-//                 tabination === index
-//                   ? 'border-gh-orange text-gh-dark font-semibold'
-//                   : 'border-transparent'
-//               }`}
-//               onClick={() => setTabination(index)}
-//             >
-//               {tab}
-//             </button>
-//           ))}
-//         </div>
-//         <main className='flex-1 p-4 flex flex-col gap-4'>
-//           {tabs[tabination] === 'Signin' && (
-//             <Input label='Username' type={'text'} placeholder='ex: john0967' />
-//           )}
-//           <Input label='Email' type={'email'} placeholder='ex: john@example.com' />
-//           <Input
-//             label='Password'
-//             type={'password'}
-//             action={{
-//               label: 'Show',
-//               onClick: () => {},
-//             }}
-//           />
-//         </main>
-//         <hr></hr>
-//         <footer className='p-4 flex flex-col gap-2'>
-//           <Button text='Login' />
-//         </footer>
-//       </section>
-//     </ModalLayout>
-//   )
-// }
 
 export default UserAuthModal
