82e20d2e05cccb04134f4bf11c4ee4065bfa3653
nuintee <nuinteedev@gmail.com>
2023-01-23 06:10:57 +0900

    add: user/[user_id] API route api/routes#75

--- src/pages/api/v1/user/[user_id].ts
+++ src/pages/api/v1/user/[user_id].ts
@@ -1,20 +1,29 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { handleRequest, userTable, Response } from '@/utils/prisma'
+
+import { userTable } from '@/features/user/prisma/userTable'
 
 // GET | PATCH | DELETE
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Response>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const user_id = req.query.user_id as string
-  switch (req.method) {
-    case 'GET':
-      await handleRequest(() => userTable.get({ id: user_id }), res)
-      break
-    case 'PATCH':
-      await handleRequest(() => userTable.patch({ id: user_id, ...req.body }), res)
-      break
-    case 'DELETE':
-      await handleRequest(() => userTable.delete({ id: user_id }), res)
-      break
-    default:
-      break
+  try {
+    switch (req.method) {
+      case 'GET':
+        const fetchedUser = await userTable.get({ id: user_id })
+        res.status(200).json(fetchedUser)
+        break
+      case 'PATCH':
+        const updatedUser = await userTable.patch({ id: user_id, ...req.body })
+        res.status(200).json(updatedUser)
+        break
+      case 'DELETE':
+        const deletedUser = await userTable.delete({ id: user_id })
+        res.status(200).json(deletedUser)
+        break
+      default:
+        res.status(405).json({ message: 'Invalid Method', code: 405, method: req.method })
+        break
+    }
+  } catch (error) {
+    res.status(500).json({ message: JSON.parse(error?.message), code: 500 })
   }
 }
