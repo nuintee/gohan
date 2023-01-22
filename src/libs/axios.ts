2900cee9a552f17c16255b23fb851f345e27b277
nuintee <nuinteedev@gmail.com>
2023-01-23 04:56:26 +0900

    add: axios config libs/axios-config#73
    closes #73

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -8,23 +8,11 @@ export const axios = Axios.create({
   timeout: AXIOS_FETCH_TIMEOUT,
 })
 
-const clampError = (error: any): BackendResponse => {
-  const { data } = error.response?.data || { data: { message: error.message } }
-  return data
-}
-
 axios.interceptors.response.use(
   (response) => {
     return response.data
   },
   (error) => {
-    const niceError = {
-      ...error,
-      response: {
-        ...error.response,
-        data: clampError(error),
-      },
-    }
-    return Promise.reject(niceError)
+    return Promise.reject(error)
   },
 )
