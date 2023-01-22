53b5ee2bacf3124a8e4f4d15e1b8242a9b1f4c85
nuintee <nuinteedev@gmail.com>
2023-01-23 01:07:17 +0900

    update: RestaurantCard width to fixed 20rem refactor/directory-structure#52

--- src/features/restaurants/components/RestaurantCard.tsx
+++ src/features/restaurants/components/RestaurantCard.tsx
@@ -15,7 +15,7 @@ const RestaurantCard = (props: RestaurantProps) => {
   const { compact, data, isLocked, isNavigating, onLike, onClick } = props
 
   const cardStyle = {
-    container: 'min-w-[20rem] rounded-md overflow-hidden bg-white relative',
+    container: 'w-[20rem] rounded-md overflow-hidden bg-white relative',
     img: `select-none max-h-52 w-full object-cover h-52`,
     closeButton: 'absolute left-[1rem] top-[1rem] outline-none z-10',
     contents: 'p-4 flex flex-col gap-4',
