ffb5917b14aafe505e394a60651d69d89773971f
nuintee <nuinteedev@gmail.com>
2023-01-23 09:11:46 +0900

    add: mocks directory mock/api#81

---
+++ src/mocks/worker.ts
@@ -0,0 +1,13 @@
+const IS_BROWSER = typeof window !== 'undefined'
+
+export const initMocks = async () => {
+  if (IS_BROWSER) {
+    const { worker } = await import('./browser')
+    worker.start()
+  } else {
+    const { server } = await import('./server')
+    server.listen()
+  }
+}
+
+// initMocks()
