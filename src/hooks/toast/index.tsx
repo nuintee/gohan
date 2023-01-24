90a0f8a93046c74d598b837804d041216780326c
nuintee <nuinteedev@gmail.com>
2023-01-25 08:43:47 +0900

    add: useToast hooks/toast#85

--- src/hooks/toast/index.tsx
+++ src/hooks/toast/index.tsx
@@ -1,7 +1,27 @@
 import { toast as t, ToastContent, ToastContentProps, ToastOptions } from 'react-toastify'
 
-const useToast = (content: ToastContent, options?: ToastOptions) => {
-  return t(content, options)
+const useToast = () => {
+  const success = (content: ToastContent, options?: ToastOptions) => {
+    return t.success(content, options)
+  }
+
+  const error = (content: ToastContent, options?: ToastOptions) => {
+    return t.error(content, options)
+  }
+
+  const info = (content: ToastContent, options?: ToastOptions) => {
+    return t.info(content, options)
+  }
+
+  const warn = (content: ToastContent, options?: ToastOptions) => {
+    return t.warn(content, options)
+  }
+
+  const normal = (content: ToastContent, options?: ToastOptions) => {
+    return t(content, options)
+  }
+
+  return { success, error, info, warn, normal }
 }
 
 export default useToast
