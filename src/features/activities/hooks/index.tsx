08ab6968378d5745b932004a49e3f0cad033026f
nuintee <nuinteedev@gmail.com>
2023-01-26 08:23:11 +0900

    add: useActivities add hooks/activities#92

--- src/features/activities/hooks/index.tsx
+++ src/features/activities/hooks/index.tsx
@@ -1,11 +1,28 @@
 // Types
 
+import { BASE_URL } from '@/config/env'
+import axios from '@/libs/axios'
+import useToast from '@/libs/react-toastify'
+import { Activity } from '@prisma/client'
+import { ZodError } from 'zod'
+
 const useActivities = () => {
-  const add = async (payload) => {}
+  const add = async (payload: Activity) => {
+    try {
+      const url = new URL(`${BASE_URL}/api/v1/activities`)
+      const { data } = await axios.post<Activity>(url.toString(), payload)
+      return data
+    } catch (error) {
+      console.error(error)
+      if (error instanceof ZodError) return useToast.info('Invalid parameters')
 
-  const get = async (activityId: string) => {}
+      useToast.error(error.message)
+    }
+  }
 
-  const update = async (activityId: string, payload) => {}
+  const get = async (activityId: string): Activity => {}
+
+  const update = async (activityId: string, payload: Activity) => {}
 
   const getUserAll = async () => {}
 
--- src/features/activities/schemas/addActivity.schema.ts
+++ src/features/activities/schemas/addActivity.schema.ts
@@ -1,6 +1,9 @@
 import { z } from 'zod'
 
-export const addActivitySchema = z.object({
-  user_id: z.string(),
-  place_id: z.string(),
+export const updateActivitySchema = z.object({
+  user_id: z.optional(z.string()),
+  place_id: z.optional(z.string()),
+  is_liked: z.optional(z.boolean()),
 })
+
+export type UpdateActivityProps = z.infer<typeof updateActivitySchema>
