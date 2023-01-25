63e7e714526589c24fdd45fbdf6be0b2560cc2be
nuintee <nuinteedev@gmail.com>
2023-01-25 10:03:27 +0900

    add: useModals hooks/modals#87

--- src/hooks/modals/index.tsx
+++ src/hooks/modals/index.tsx
@@ -5,15 +5,29 @@ import { useRecoilState } from 'recoil'
 const useModals = () => {
   const [modals, setModals] = useRecoilState(modalState)
 
-  const open = (key: typeof modals.modalKey[number]) => {
-    if (modals.modalKey.includes(key)) return
+  /**
+   * Checks if the target modal is open by given key
+   */
+  const isOpen = (key: typeof modals.modalKey[number]) => {
+    return modals.modalKey.includes(key)
+  }
 
-    setModals((prev) => ({ ...prev, modalKey: [...prev.modalKey, key] }))
+  const _appendKey = (key: typeof modals.modalKey[number]) => {
+    setModals((prev) => ({ ...prev, modalKey: [...prev.modalKey.filter((v) => v !== key), key] })) // appending while keeping unique
+  }
+
+  const _removeKey = (key: typeof modals.modalKey[number]) => {
+    setModals((prev) => ({ ...prev, modalKey: prev.modalKey.filter((v) => v != key) }))
+  }
+
+  const open = (key: typeof modals.modalKey[number]) => {
+    if (isOpen(key)) return
+    _appendKey(key)
   }
 
   const close = (key: typeof modals.modalKey[number]) => {
-    if (!modals.modalKey.includes(key)) return
-    setModals((prev) => ({ ...prev, modalKey: prev.modalKey.filter((v) => v != key) }))
+    if (!isOpen(key)) return
+    _removeKey(key)
   }
 
   const toggle = (key: typeof modals.modalKey[number]) => {
@@ -28,7 +42,7 @@ const useModals = () => {
     setModals((prev) => ({ ...prev, modalKey: [''] }))
   }
 
-  return { open, close, toggle, closeAll }
+  return { open, close, toggle, closeAll, isOpen }
 }
 
 export default useModals
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { ToastCatcher } from '@/components/ui'
 import MapBox from '@/features/mapbox/components/MapBox'
+import useModals from '@/hooks/modals'
 
 const Index = () => {
+  const { isOpen } = useModals()
   return (
     <>
-      <div>
+      <div className='flex flex-col gap-4'>
         <MapBox />
       </div>
       <ToastCatcher position='top-center' />
