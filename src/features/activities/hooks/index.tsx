b1774550a78d0b9f70cbc152e7784bc348cfdea2
nuintee <nuinteedev@gmail.com>
2023-01-26 09:43:39 +0900

    update: useActivities hook with _handleFetchActivities hooks/activities#92

--- src/features/activities/hooks/index.tsx
+++ src/features/activities/hooks/index.tsx
@@ -19,67 +19,83 @@ const useActivities = () => {
       return data
     } catch (error) {
       if (error instanceof ZodError) return useToast.info('Invalid parameters')
-      useToast.error(error.message)
+      return useToast.error(error.message)
     }
   }
 
   const add = async (payload: AddActivityProps) => {
     const url = new URL(`${BASE_URL}/api/v1/activities`)
-    const fetcher = axios.post<Activity>(url.toString(), payload)
-    const data = await _handleFetchActivities(fetcher)
-    return data
+    const fetcher = async () => axios.post<Activity>(url.toString(), payload)
+    const response = await _handleFetchActivities(fetcher)
+    return response
   }
 
   const get = async (activityId: string) => {
-    try {
-      if (status !== 'authenticated') throw new Error('Must be authed to operate this action')
-      const url = new URL(`${BASE_URL}/api/v1/activity/${activityId}`)
-      const { data } = await axios.get<ActivityResolved>(url.toString())
-      return data
-    } catch (error) {
-      console.error(error)
-      if (error instanceof ZodError) return useToast.info('Invalid parameters')
-      useToast.error(error.message)
-    }
+    // try {
+    //   if (status !== 'authenticated') throw new Error('Must be authed to operate this action')
+    //   const url = new URL(`${BASE_URL}/api/v1/activity/${activityId}`)
+    //   const { data } = await axios.get<ActivityResolved>(url.toString())
+    //   return data
+    // } catch (error) {
+    //   console.error(error)
+    //   if (error instanceof ZodError) return useToast.info('Invalid parameters')
+    //   useToast.error(error.message)
+    // }
+    const url = new URL(`${BASE_URL}/api/v1/activity/${activityId}`)
+    const fetcher = async () => axios.get<ActivityResolved>(url.toString())
+    const response = await _handleFetchActivities(fetcher)
+    return response
   }
 
   const update = async (activityId: string, payload?: UpdateActivityProps) => {
-    try {
-      if (status !== 'authenticated') throw new Error('Must be authed to operate this action')
-      const url = new URL(`${BASE_URL}/api/v1/activity/${activityId}`)
-      const { data } = await axios.patch<Activity>(url.toString(), payload)
-      return data
-    } catch (error) {
-      console.error(error)
-      if (error instanceof ZodError) return useToast.info('Invalid parameters')
-      useToast.error(error.message)
-    }
+    // try {
+    //   if (status !== 'authenticated') throw new Error('Must be authed to operate this action')
+    //   const url = new URL(`${BASE_URL}/api/v1/activity/${activityId}`)
+    //   const { data } = await axios.patch<Activity>(url.toString(), payload)
+    //   return data
+    // } catch (error) {
+    //   console.error(error)
+    //   if (error instanceof ZodError) return useToast.info('Invalid parameters')
+    //   useToast.error(error.message)
+    // }
+    const url = new URL(`${BASE_URL}/api/v1/activity/${activityId}`)
+    const fetcher = async () => axios.patch<Activity>(url.toString(), payload)
+    const response = await _handleFetchActivities(fetcher)
+    return response
   }
 
   const getUserAll = async () => {
-    try {
-      if (status !== 'authenticated') throw new Error('Must be authed to operate this action')
-      const url = new URL(`${BASE_URL}/api/v1/activities/user/${session?.user?.id}`)
-      const { data } = await axios.get<ActivityResolved[]>(url.toString())
-      return data
-    } catch (error) {
-      console.error(error)
-      if (error instanceof ZodError) return useToast.info('Invalid parameters')
-      useToast.error(error.message)
-    }
+    // try {
+    //   if (status !== 'authenticated') throw new Error('Must be authed to operate this action')
+    //   const url = new URL(`${BASE_URL}/api/v1/activities/user/${session?.user?.id}`)
+    //   const { data } = await axios.get<ActivityResolved[]>(url.toString())
+    //   return data
+    // } catch (error) {
+    //   console.error(error)
+    //   if (error instanceof ZodError) return useToast.info('Invalid parameters')
+    //   useToast.error(error.message)
+    // }
+    const url = new URL(`${BASE_URL}/api/v1/activities/user/${session?.user?.id}`)
+    const fetcher = async () => axios.get<ActivityResolved[]>(url.toString())
+    const response = await _handleFetchActivities(fetcher)
+    return response
   }
 
   const remove = async (activityId: string) => {
-    try {
-      if (status !== 'authenticated') throw new Error('Must be authed to operate this action')
-      const url = new URL(`${BASE_URL}/api/v1/activitiy/${activityId}`)
-      const { data } = await axios.delete<Activity>(url.toString())
-      return data
-    } catch (error) {
-      console.error(error)
-      if (error instanceof ZodError) return useToast.info('Invalid parameters')
-      useToast.error(error.message)
-    }
+    // try {
+    //   if (status !== 'authenticated') throw new Error('Must be authed to operate this action')
+    //   const url = new URL(`${BASE_URL}/api/v1/activitiy/${activityId}`)
+    //   const { data } = await axios.delete<Activity>(url.toString())
+    //   return data
+    // } catch (error) {
+    //   console.error(error)
+    //   if (error instanceof ZodError) return useToast.info('Invalid parameters')
+    //   useToast.error(error.message)
+    // }
+    const url = new URL(`${BASE_URL}/api/v1/activitiy/${activityId}`)
+    const fetcher = async () => axios.delete<Activity>(url.toString())
+    const response = await _handleFetchActivities(fetcher)
+    return response
   }
 
   return { add, get, getUserAll, remove, update }
