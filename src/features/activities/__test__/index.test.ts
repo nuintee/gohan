833eb180cc3366852f6163212a379a7db1bf8993
nuintee <nuinteedev@gmail.com>
2023-01-27 01:15:46 +0900

    add: SessionProivder wrapper in activities test hooks/activities#92

--- src/features/activities/__test__/index.test.ts
+++ src/features/activities/__test__/index.test.ts
@@ -1,9 +1,13 @@
 import { renderHook } from '@testing-library/react'
+import { Session } from 'next-auth'
 import useActivities from '../hooks'
+import { SessionProvider } from 'next-auth/react'
 
 describe('useHoge Custom Hooks Test', () => {
   test('Custom Hooks の返り値は hoge になること', () => {
-    const { result } = renderHook(() => useActivities().get(''))
+    const { result } = renderHook(() => useActivities().get(''), {
+      wrapper: SessionProvider,
+    })
     console.log(result)
   })
 })
