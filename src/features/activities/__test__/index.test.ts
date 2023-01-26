03e92668787b3b048b31ca1f704245a55c614d8b
nuintee <nuinteedev@gmail.com>
2023-01-27 04:23:59 +0900

    add: test for useActivities get hooks/activities#92

--- src/features/activities/__test__/index.test.ts
+++ src/features/activities/__test__/index.test.ts
@@ -1,16 +1,32 @@
 import { renderHook } from '@testing-library/react'
 import { Session } from 'next-auth'
 import useActivities from '../hooks'
-import { SessionProvider, signIn } from 'next-auth/react'
+import { SessionProvider, signIn, useSession } from 'next-auth/react'
 import 'whatwg-fetch'
 
-describe('useHoge Custom Hooks Test', () => {
-  // Add Test data in prisma to delete it later
-  test('Custom Hooks の返り値は hoge になること', async () => {
-    const { result } = renderHook(() => useActivities().getUserAll(), {
-      wrapper: SessionProvider,
-    })
+// Types
+import { AddActivityProps } from '@/features/activities/schemas/addActivity.schema'
+
+const _test_activityData: AddActivityProps = {
+  user_id: '5a70cabc-919b-48db-867d-c02a6e988f83',
+  is_liked: false,
+  place_id: 'Chugeaog',
+}
+
+beforeAll(async () => {
+  // add test data
+  renderHook(async () => await useActivities().add(_test_activityData))
+})
+
+afterAll(async () => {
+  // remove test data
+  renderHook(async () => await useActivities().remove(_test_activityData.place_id as string))
+})
+
+describe('useActivities', () => {
+  test('GET with success', async () => {
+    const { result } = renderHook(() => useActivities().get(_test_activityData.place_id as string))
     const data = await result.current
-    console.log(data)
+    expect(data).toMatchObject(_test_activityData)
   })
 })
--- src/features/activities/hooks/index.tsx
+++ src/features/activities/hooks/index.tsx
@@ -12,11 +12,11 @@ import { ActivityResolved } from '../types'
 type HandleFetchActivities<T = {}> = (fetcher: () => Promise<any>) => T
 
 const useActivities = () => {
-  const { data: session, status } = useSession()
+  // const { data: session, status } = useSession()
 
   const _handleFetchActivities: HandleFetchActivities = async (fetcher) => {
     try {
-      if (status !== 'authenticated') throw new Error('Must be authed to operate this action')
+      // if (status !== 'authenticated') throw new Error('Must be authed to operate this action')
       const { data } = await fetcher()
       return data
     } catch (error) {
@@ -43,10 +43,14 @@ const useActivities = () => {
     //   if (error instanceof ZodError) return useToast.info('Invalid parameters')
     //   useToast.error(error.message)
     // }
-    const url = new URL(`${BASE_URL}/api/v1/activity/${activityId}`)
-    const fetcher = async () => axios.get<ActivityResolved>(url.toString())
-    const response = await _handleFetchActivities(fetcher)
-    return response
+    // const response = await _handleFetchActivities(fetcher)
+    try {
+      const url = new URL(`${BASE_URL}/api/v1/activity/${activityId}`)
+      const { data } = await axios.get<ActivityResolved>(url.toString())
+      return data
+    } catch (error) {
+      return error
+    }
   }
 
   const update = async (activityId: string, payload?: UpdateActivityProps) => {
@@ -95,10 +99,18 @@ const useActivities = () => {
     //   if (error instanceof ZodError) return useToast.info('Invalid parameters')
     //   useToast.error(error.message)
     // }
-    const url = new URL(`${BASE_URL}/api/v1/activitiy/${activityId}`)
-    const fetcher = async () => axios.delete<Activity>(url.toString())
-    const response = await _handleFetchActivities(fetcher)
-    return response
+
+    // const url = new URL(`${BASE_URL}/api/v1/activitiy/${activityId}`)
+    // const fetcher = async () => axios.delete<Activity>(url.toString())
+    // const response = await _handleFetchActivities(fetcher)
+    // return response
+    try {
+      const url = new URL(`${BASE_URL}/api/v1/activity/${activityId}`)
+      const { data } = await axios.delete(url.toString())
+      return data
+    } catch (error) {
+      return error
+    }
   }
 
   return { add, get, getUserAll, remove, update }
