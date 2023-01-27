6cf45153d1df05cebb93a7727685c51d499546f5
nuintee <nuinteedev@gmail.com>
2023-01-27 11:01:01 +0900

    add: delete activity test hooks/activities#92

--- src/features/activities/__test__/index.test.tsx
+++ src/features/activities/__test__/index.test.tsx
@@ -24,94 +24,10 @@ const _test_activityData: AddActivityProps = {
   place_id: '_TEST_PLACE_ID',
 }
 
-// beforeAll(async () => {
-//   // add test data
-//   const { result } = renderHook(() => useActivities().add(_test_activityData), { wrapper })
-//   await waitFor(async () => {
-//     const added = await result.current.mutateAsync()
-//     expect(added).toMatchObject(_test_activityData)
-//   })
-// })
+const _updateField = {
+  is_liked: true,
+}
 
-// afterAll(async () => {
-//   // add test data
-//   const { result } = renderHook(
-//     () => useActivities().remove(_test_activityData.place_id as string),
-//     { wrapper },
-//   )
-//   await waitFor(async () => {
-//     const cleared = await result.current.mutateAsync()
-//     expect(cleared).toMatchObject(_test_activityData)
-//   })
-// })
-
-// describe('useActivities', () => {
-//   test('GET with success', async () => {
-//     const { result } = renderHook(
-//       () => useActivities().get(_test_activityData.place_id as string),
-//       {
-//         wrapper,
-//       },
-//     )
-//     await waitFor(() => {
-//       if (!result.current.isSuccess) throw new Error('Wait')
-//       expect(result.current.data).toMatchObject(_test_activityData)
-//     })
-//   })
-
-//   test('GET with error', async () => {
-//     const { result } = renderHook(() => useActivities().get(''), {
-//       wrapper,
-//     })
-//     await waitFor(() => {
-//       if (result.current.isSuccess) throw new Error('Wait')
-//       expect(result.current.isError).toBe(true)
-//     })
-//   })
-
-//   test('POST with success', async () => {
-//     const _post_activityData = {
-//       user_id: '5a70cabc-919b-48db-867d-c02a6e988f83',
-//       is_liked: true,
-//       place_id: '_TEMP_PLACE_ID',
-//     }
-
-//     try {
-//       const { result } = renderHook(() => useActivities().add(_post_activityData), { wrapper })
-//       const added = await result.current.mutateAsync()
-//       await waitFor(async () => {
-//         expect(added).toMatchObject(_post_activityData)
-//       })
-//     } finally {
-//       const { result } = renderHook(
-//         () => useActivities().remove(_post_activityData.place_id as string),
-//         { wrapper },
-//       )
-//       await result.current.mutateAsync() // clearing post data
-//     }
-//   })
-
-//   test('POST with error', async () => {
-//     // Already existing id
-//     const { result } = renderHook(() => useActivities().add(_test_activityData), { wrapper })
-//     await waitFor(async () => {
-//       expect(result.current.status).not.toBe(200)
-//     })
-//   })
-
-//   test('DELETE with success', async () => {
-//     const _delete_activityData = {
-//       user_id: '5a70cabc-919b-48db-867d-c02a6e988f83',
-//       is_liked: true,
-//       place_id: '_TEMP_PLACE_ID',
-//     }
-//     // add Before delete
-//     const { result } = renderHook(() => useActivities().add(_test_activityData), { wrapper })
-//     await waitFor(async () => {
-//       expect(result.current.status).not.toBe(200)
-//     })
-//   })
-// })
 describe('useActivities', () => {
   // POST
   test('POST with success', async () => {
@@ -153,18 +69,45 @@ describe('useActivities', () => {
 
   // PATCH
   test('PATCH with success', async () => {
-    const updateField = {
-      is_liked: true,
-    }
     const { result } = renderHook(
-      () => useActivities().update(_test_activityData.place_id as string, updateField),
+      () => useActivities().update(_test_activityData.place_id as string, _updateField),
       {
         wrapper,
       },
     )
     const updated = await result.current.mutateAsync()
     await waitFor(async () => {
-      expect(updated).toMatchObject({ ..._test_activityData, ...updateField })
+      expect(updated).toMatchObject({ ..._test_activityData, ..._updateField })
+    })
+  })
+
+  test('PATCH with error', async () => {
+    const { result } = renderHook(() => useActivities().update(''), {
+      wrapper,
+    })
+    await waitFor(() => {
+      expect(result.current.status).not.toBe(200)
+    })
+  })
+
+  // DELETE
+  test('DELETE with error', async () => {
+    const { result } = renderHook(() => useActivities().remove(''), {
+      wrapper,
+    })
+    await waitFor(() => {
+      expect(result.current.status).not.toBe(200)
+    })
+  })
+
+  test('DELETE with success', async () => {
+    const { result } = renderHook(
+      () => useActivities().remove(_test_activityData.place_id as string),
+      { wrapper },
+    )
+    const removed = await result.current.mutateAsync()
+    await waitFor(async () => {
+      expect(removed).toMatchObject({ ..._test_activityData, ..._updateField })
     })
   })
 })
