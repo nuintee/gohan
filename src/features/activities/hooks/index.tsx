93c1aa78513177c62e730dd459f5ffd17d1d5eb0
nuintee <nuinteedev@gmail.com>
2023-01-27 11:05:13 +0900

    add: activating toast on error hooks/activities#92

--- src/features/activities/hooks/index.tsx
+++ src/features/activities/hooks/index.tsx
@@ -1,5 +1,3 @@
-// Types
-
 import { BASE_URL } from '@/config/env'
 import axios from '@/libs/axios'
 import useToast from '@/libs/react-toastify'
@@ -10,63 +8,57 @@ import { ZodError } from 'zod'
 import { AddActivityProps, UpdateActivityProps } from '../schemas/addActivity.schema'
 import { ActivityResolved } from '../types'
 
-type HandleFetchActivities<T = {}> = (fetcher: () => Promise<any>) => T
-
 const useActivities = () => {
   // const { data: session, status } = useSession()
 
-  const _handleFetchActivities: HandleFetchActivities = async (fetcher) => {
-    try {
-      // if (status !== 'authenticated') throw new Error('Must be authed to operate this action')
-      const { data } = await fetcher()
-      return data
-    } catch (error) {
-      if (error instanceof ZodError) return useToast.info('Invalid parameters')
-      return useToast.error(error.message)
-    }
-  }
-
   const add = (payload: AddActivityProps) => {
     console.log(`${BASE_URL}/api/v1/activities`)
-    return useMutation(() =>
-      axios.post(`${BASE_URL}/api/v1/activities`, payload).then((res) => res.data),
+    return useMutation(
+      () => axios.post(`${BASE_URL}/api/v1/activities`, payload).then((res) => res.data),
+      {
+        onError: (error) => {
+          useToast.error(error.message)
+        },
+      },
     )
   }
 
   const get = (activityId: string) => {
-    // const url = new URL(`${BASE_URL}/api/v1/activity/${activityId}`)
-    // const fetcher = async () => axios.get<Activity>(url.toString())
-
-    // const response = useQuery({
-    //   queryKey: ['activity'],
-    //   queryFn: () => 'Hello',
-    // })
-    // return response
-
-    // return useQuery({
-    //   queryKey: ['activity'],
-    //   queryFn: () => fetch(`${BASE_URL}/api/v1/activity/${activityId}`).then((data) => data.json()),
-    // })
-    return useQuery([activityId], () =>
-      axios.get(`${BASE_URL}/api/v1/activity/${activityId}`).then((res) => res.data),
+    return useQuery(
+      [activityId],
+      () => axios.get(`${BASE_URL}/api/v1/activity/${activityId}`).then((res) => res.data),
+      {
+        onError: (error) => {
+          useToast.error(error.message)
+        },
+      },
     )
   }
 
   const update = (activityId: string, payload?: UpdateActivityProps) => {
-    return useMutation(() =>
-      axios.patch(`${BASE_URL}/api/v1/activity/${activityId}`, payload).then((res) => res.data),
+    return useMutation(
+      () =>
+        axios.patch(`${BASE_URL}/api/v1/activity/${activityId}`, payload).then((res) => res.data),
+      {
+        onError: (error) => {
+          useToast.error(error.message)
+        },
+      },
     )
   }
-
-  const getUserAll = async () => {}
 
   const remove = (activityId: string) => {
-    return useMutation(() =>
-      axios.delete(`${BASE_URL}/api/v1/activity/${activityId}`).then((res) => res.data),
+    return useMutation(
+      () => axios.delete(`${BASE_URL}/api/v1/activity/${activityId}`).then((res) => res.data),
+      {
+        onError: (error) => {
+          useToast.error(error.message)
+        },
+      },
     )
   }
 
-  return { add, get, getUserAll, remove, update }
+  return { add, get, remove, update }
 }
 
 export default useActivities
