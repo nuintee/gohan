1053d175da3123bfecf183c498ca9e031e2d5f1f
nuintee <nuinteedev@gmail.com>
2023-01-29 08:04:27 +0900

    update: unused code in useMapBox hooks/directions#90

--- src/features/mapbox/hooks/index.tsx
+++ src/features/mapbox/hooks/index.tsx
@@ -6,13 +6,10 @@ import { useRecoilState } from 'recoil'
 import { mapBoxState } from '../stores'
 
 const useMapBox = () => {
-  // const { hasDirections, get, set, directions } = useDirections()
   const [mapbox, setMapBox] = useRecoilState(mapBoxState)
 
   const updateCoords = (coords: GeolocationCoordinates) => {
     setMapBox((prev) => ({ ...prev, coords }))
-    // update route if exists
-    // if (!hasDirections) return
   }
 
   const updateViewState = (viewState: ViewState) => {
