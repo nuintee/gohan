6c9c29c8c980f6ae70a79d831fd6d7c33badc785
nuintee <nuinteedev@gmail.com>
2023-01-29 21:53:51 +0900

    add: update function in useUser hooks/user#101

--- src/features/user/hooks/index.tsx
+++ src/features/user/hooks/index.tsx
@@ -1,15 +1,31 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { useSession } from 'next-auth/react'
+import axios from '@/libs/axios'
 
 // types
 import { UpdateUserProps } from '@/features/user/schema'
+import useToast from '@/libs/react-toastify'
+
+// env
+import { BASE_URL } from '@/config/env'
+const BASE_KEY = ['user']
 
 const useUser = () => {
+  const quryClient = useQueryClient()
   const { data: seesion, status } = useSession()
 
   const update = (payload: UpdateUserProps) => {
-    return useMutation(() => {}, {})
+    return useMutation(() => axios.patch(`${BASE_URL}/api/v1/user/${seesion?.user.id}`, payload), {
+      onSuccess: () => {
+        quryClient.invalidateQueries([BASE_KEY])
+      },
+      onError: (error) => {
+        useToast.error(error)
+      },
+    })
   }
+
+  return update
 }
 
 export default useUser
