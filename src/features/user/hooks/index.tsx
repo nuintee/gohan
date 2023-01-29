2f47515bddad70123b8260f43313d1109e4b10ba
nuintee <nuinteedev@gmail.com>
2023-01-29 22:21:03 +0900

    update: get Users with enabled depending on auth state if true hooks/user#101

--- src/features/user/hooks/index.tsx
+++ src/features/user/hooks/index.tsx
@@ -1,31 +1,44 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { useSession } from 'next-auth/react'
 import axios from '@/libs/axios'
 
 // types
 import { UpdateUserProps } from '@/features/user/schema'
-import useToast from '@/libs/react-toastify'
-
 // env
 import { BASE_URL } from '@/config/env'
-const BASE_KEY = ['user']
+import useToast from '@/libs/react-toastify'
+const BASE_KEY = 'user'
 
 const useUser = () => {
   const quryClient = useQueryClient()
   const { data: seesion, status } = useSession()
+  const user = seesion?.user
 
   const update = (payload: UpdateUserProps) => {
-    return useMutation(() => axios.patch(`${BASE_URL}/api/v1/user/${seesion?.user.id}`, payload), {
-      onSuccess: () => {
-        quryClient.invalidateQueries([BASE_KEY])
+    return useMutation(
+      () => axios.patch(`${BASE_URL}/api/v1/user/${user?.id}`, payload).then((res) => res.data),
+      {
+        onSuccess: (data) => {
+          quryClient.invalidateQueries([BASE_KEY])
+        },
+        onError: (error) => {
+          useToast.error(error.message)
+        },
       },
-      onError: (error) => {
-        useToast.error(error)
+    )
+  }
+
+  const get = () => {
+    return useQuery({
+      queryKey: [BASE_KEY],
+      queryFn: async () => {
+        return user
       },
+      enabled: status === 'authenticated',
     })
   }
 
-  return update
+  return { update, get }
 }
 
 export default useUser
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,34 @@ import useDirections from '@/features/directions/hooks'
 import MapBox from '@/features/mapbox/components/MapBox'
 import useMapBox from '@/features/mapbox/hooks'
 import { mapBoxState } from '@/features/mapbox/stores'
+import useUser from '@/features/user/hooks'
 import { useEffect, useState } from 'react'
 import { useRecoilValue } from 'recoil'
 
+import { signOut, useSession, signIn } from 'next-auth/react'
+
 const Index = () => {
+  const { status } = useSession()
+  const { update, get } = useUser()
+  const updateUser = update({
+    email: 'ex@example.com',
+  })
+
+  const getUser = get()
+
   return (
     <>
       <div className='flex flex-col gap-4'>
-        <MapBox />
+        <div className='absolute top-0 left-0'>
+          <p>{JSON.stringify(getUser?.data)}</p>
+          <button onClick={() => updateUser.mutate()}>Update</button>
+          <button
+            onClick={status === 'authenticated' ? () => signOut() : () => signIn('credentials')}
+          >
+            {status === 'authenticated' ? 'signout' : 'signin'}
+          </button>
+        </div>
+        {/* <MapBox /> */}
       </div>
       <ToastCatcher position='top-center' />
     </>
