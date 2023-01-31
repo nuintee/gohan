0f056720180637f70147786acbf32c8249085338
nuintee <nuinteedev@gmail.com>
2023-01-31 17:19:49 +0300

    add: default UI ui/index#108

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { ToastCatcher } from '@/components/ui'
+import { GohanButton, ToastCatcher } from '@/components/ui'
 import useDirections from '@/features/directions/hooks'
 import MapBox from '@/features/mapbox/components/MapBox'
 import useMapBox from '@/features/mapbox/hooks'
@@ -11,12 +11,24 @@ import { signOut, useSession, signIn } from 'next-auth/react'
 
 // ENV
 import { AUTH0_CLIENT_ID, AUTH0_DOMAIN, BASE_URL } from '@/config/env'
+import User from '@/features/user/components/User'
+import useModals from '@/hooks/modals'
+import UserAuthConsentDialog from '@/features/user/components/UserAuthConsentDialog'
+import AcitvityButton from '@/features/activities/components/ActivityButton'
+import useActivities from '@/features/activities/hooks'
 
 const Index = () => {
   return (
     <>
       <div className='flex flex-col gap-4'>
+        <section className='absolute top-0 left-0 z-[1] w-full p-4 flex gap-4 justify-between'>
+          <User />
+          <AcitvityButton />
+        </section>
         <MapBox />
+        <section className='absolute bottom-0 left-0 z-[1] w-full flex items-center justify-center p-4'>
+          <GohanButton />
+        </section>
       </div>
       <ToastCatcher position='top-center' />
     </>
