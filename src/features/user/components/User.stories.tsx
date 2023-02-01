4e840d94ee96c6f727ac8c2161a9345daa6b7905
nuintee <nuinteedev@gmail.com>
2023-02-01 22:50:32 +0300

    remove: component decorators dev

--- src/features/user/components/User.stories.tsx
+++ src/features/user/components/User.stories.tsx
@@ -7,27 +7,9 @@ import User from './User'
 // Data
 import { user } from '@/data/user'
 
-// Wrapper
-import { SessionProvider } from 'next-auth/react'
-import { RecoilRoot } from 'recoil'
-
-const decorator = (Story, ctx, authed) => {
-  return (
-    <SessionProvider session={authed && { user }}>
-      <RecoilRoot>
-        <Story />
-      </RecoilRoot>
-    </SessionProvider>
-  )
-}
-
-const defaultDecorators = [(Story, ctx) => decorator(Story, ctx, false)]
-const authedDecorators = [(Story, ctx) => decorator(Story, ctx, true)]
-
 export default {
   title: 'Features/Users/User',
   component: User,
-  decorators: defaultDecorators,
 } as ComponentMeta<typeof User>
 
 const Template: ComponentStory<typeof User> = (args) => {
@@ -36,5 +18,3 @@ const Template: ComponentStory<typeof User> = (args) => {
 
 export const Guest = Template.bind({})
 export const Authed = Template.bind({})
-
-Authed.decorators = authedDecorators
--- src/features/user/components/UserAuthConsentDialog.stories.tsx
+++ src/features/user/components/UserAuthConsentDialog.stories.tsx
@@ -3,25 +3,10 @@ import React, { ComponentProps } from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 
 import UserAuthConsentDialog from './UserAuthConsentDialog'
-import { SessionProvider } from 'next-auth/react'
-import { RecoilRoot } from 'recoil'
-
-const decorator = (Story, ctx) => {
-  return (
-    <SessionProvider>
-      <RecoilRoot>
-        <Story />
-      </RecoilRoot>
-    </SessionProvider>
-  )
-}
-
-const defaultDecorators = [(Story, ctx) => decorator(Story, ctx, false)]
 
 export default {
   title: 'Features/Users/Modals/UserAuthConsentDialog',
   component: UserAuthConsentDialog,
-  decorators: defaultDecorators,
 } as ComponentMeta<typeof UserAuthConsentDialog>
 
 const Template: ComponentStory<typeof UserAuthConsentDialog> = (args) => {
