baaa21cba70d807ed67289ae9d40988691c5d536
nuintee <nuinteedev@gmail.com>
2023-02-01 22:16:57 +0300

    update: user with default value dev

--- src/features/user/components/User.tsx
+++ src/features/user/components/User.tsx
@@ -9,21 +9,24 @@ import useModals from '@/hooks/modals'
 
 type UserProps = {
   isLoading?: boolean
+  session: SessionContextValue
+  onClick?: MouseEventHandler<HTMLButtonElement>
 }
 
 const User = (props: UserProps) => {
-  const { isLoading: loading } = props
-
   // Contexts
-  const { status, data: session } = useSession()
+  const { status, data } = useSession()
   const { open } = useModals()
-
-  // flag
-  const isLoading = loading || status === 'loading'
 
   const handleOnClick = () => {
     open(status === 'authenticated' ? 'usersettings' : 'userauth')
   }
+
+  const {
+    isLoading = status === 'loading',
+    session = { ...data, status },
+    onClick = handleOnClick,
+  } = props
 
   const feedBack = !isLoading && 'active:scale-90 cursor-pointer active:opacity-90'
 
@@ -33,7 +36,7 @@ const User = (props: UserProps) => {
       : `h-12 min-w-[6rem] rounded-full p-4 flex items-center justify-center border-2 bg-gh-white ${feedBack}`
 
   return (
-    <button className={theme} onClick={handleOnClick}>
+    <button className={theme} onClick={onClick}>
       {status === 'authenticated' ? (
         <img
           src={`https://ui-avatars.com/api/?name=${session.user?.name}&background=random`}
