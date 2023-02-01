27e5f5f654a2cd188b4c75cae80bf36ae6fc4ffe
nuintee <nuinteedev@gmail.com>
2023-02-01 22:28:33 +0300

    udpate: activity panel dev

--- src/features/activities/components/ActivityPanel.tsx
+++ src/features/activities/components/ActivityPanel.tsx
@@ -32,6 +32,8 @@ const placeholder_data = Array.from(Array(100).keys()).map((v) => ({
 
 type Props = {
   isOpen?: boolean
+  data?: ActivityResolved[]
+  onClose?: React.MouseEventHandler<HTMLButtonElement>
 }
 
 type ListProps = {
@@ -67,19 +69,24 @@ const List = (props: ListProps) => {
 }
 
 const ActivityPanel = (props: Props) => {
-  const { isOpen } = props
   const { getUserAll, update, isPanelOpen, closePanel } = useActivities()
-  const { data } = getUserAll()
+  const { data: userAllActivities } = getUserAll()
   const { status } = useSession()
-  const openState = isOpen ?? isPanelOpen
-  const slideIn = openState ? '-transform-x-full' : 'translate-x-full'
   const updateActity = update()
+
+  const {
+    isOpen = isPanelOpen ?? false,
+    onClose = closePanel,
+    data = userAllActivities ?? [],
+  } = props
+
+  const slideIn = isOpen ? '-transform-x-full' : 'translate-x-full'
 
   return (
     <div
       className={`absolute top-0 right-0 h-screen bg-white flex flex-col min-w-[20rem] w-fit duration-700 ease-in-out rounded-tl-md rounded-bl-md z-[1] ${slideIn}`}
     >
-      <Header title={'ActivityPanel'} onClose={closePanel} />
+      <Header title={'ActivityPanel'} onClose={onClose} />
       <List activities={data} isLocked={status === 'unauthenticated'} updater={updateActity} />
     </div>
   )
