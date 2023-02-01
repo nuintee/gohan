cd7212dae742ee896ec3bb19a1f65c907b58c8c6
nuintee <nuinteedev@gmail.com>
2023-02-01 04:33:23 +0300

    update: activities hanlder features/activities#113

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -35,6 +35,6 @@ export const handlers = [
   rest.get(`${BASE_PATH}/restaurants`, restaurantPlacesAPIHandler),
   rest.get(`${BASE_PATH}/restaurants/:place_id`, restaurantDetailsAPIHandler),
   rest.get(`${BASE_PATH}/directions`, directionsAPIHandler),
-  rest.get(`${BASE_PATH}/activities/user/:user_id`, userActivitiesHandler),
-  rest.get(`${BASE_PATH}/activity/:activity_id`, activityHandler),
+  rest.all(`${BASE_PATH}/activities/user/:user_id`, userActivitiesHandler),
+  rest.all(`${BASE_PATH}/activity/:activity_id`, activityHandler),
 ]
