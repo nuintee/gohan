54dfe11e52592e485c527ed241c4998674a40b8f
nuintee <nuinteedev@gmail.com>
2023-02-01 04:27:41 +0300

    add: activitiesHandlers features/activities#113

--- src/mocks/handlers/activitiesHandlers.ts
+++ src/mocks/handlers/activitiesHandlers.ts
@@ -1,20 +1,51 @@
+import { Activity } from '@prisma/client'
 import { DefaultBodyType, PathParams, ResponseComposition, RestContext, RestRequest } from 'msw'
+
+// Data
+import { details as detailsData } from '@/data/details'
+import { activities } from '@/data/activities'
+
+// Schema
+import { Schema } from '@/features/restaurants/schemas/getRestaurantDetails.schema'
+import { ActivityResolved } from '@/features/activities/types'
 
 export const userActivitiesHandler = async (
   req: RestRequest<never, PathParams<string>>,
   res: ResponseComposition<DefaultBodyType>,
   ctx: RestContext,
 ) => {
-  //   function _findOneById(place_id: string) {
-  //     const found = place_id ? places.results.find((v) => v.place_id === place_id) : places[0]
-  //     return found
-  //   }
+  const user_id = req.params.user_id as string
+  const details = req?.url.searchParams.get('details')
+  const onlyNeeded = req?.url.searchParams.get('onlyNeeded')
+
+  async function _findUserAllById(user_id: string) {
+    const found = activities.filter((activity) => activity.userId === user_id)
+    return found || []
+  }
+
+  async function _findDetailsById(place_id: string) {
+    return detailsData.result(place_id)
+  }
 
   try {
-    // const { place_id } = await DetailsAPI.Schema.parse({ place_id: req.params?.place_id })
-    // const data = _findOneById(place_id)
+    const fetchedUserActivities = await _findUserAllById(user_id)
+    let detailedActivities: Activity[] = []
 
-    return res(ctx.status(200), ctx.json({}))
+    if (details) {
+      detailedActivities = await Promise.all(
+        fetchedUserActivities.map(async (activity) => {
+          const data = await _findDetailsById(activity.place_id)
+          return data
+        }),
+      )
+    }
+
+    const mergedActivities = fetchedUserActivities.map((activity, index) => ({
+      ...activity,
+      ...(detailedActivities.length > 0 && { ...detailedActivities[index] }),
+    }))
+
+    return res(ctx.status(200), ctx.json(mergedActivities))
   } catch (error) {
     return res(
       ctx.status(500),
@@ -31,16 +62,27 @@ export const activityHandler = async (
   res: ResponseComposition<DefaultBodyType>,
   ctx: RestContext,
 ) => {
-  //   function _findOneById(place_id: string) {
-  //     const found = place_id ? places.results.find((v) => v.place_id === place_id) : places[0]
-  //     return found
-  //   }
+  const activity_id = req?.params.activity_id as string
+  const details = req?.url.searchParams.get('details')
+
+  function _findOneById(activity_id: string): ActivityResolved | {} {
+    const found = activities.find((activity) => activity.id === activity_id)
+    return found || {}
+  }
+
+  async function _findDetailsById(place_id: string) {
+    return detailsData.result(place_id)
+  }
 
   try {
-    // const { place_id } = await DetailsAPI.Schema.parse({ place_id: req.params?.place_id })
-    // const data = _findOneById(place_id)
+    const foundActivity = _findOneById(activity_id)
+    let detaildActivity = {}
 
-    return res(ctx.status(200), ctx.json({}))
+    if (details) {
+      detaildActivity = _findDetailsById(foundActivity.place_id)
+    }
+
+    return res(ctx.status(200), ctx.json({ ...foundActivity, ...detaildActivity }))
   } catch (error) {
     return res(
       ctx.status(500),
