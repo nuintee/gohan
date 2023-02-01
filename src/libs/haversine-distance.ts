0c30a1b113814e9978063ae11cb4dd76e133c2c2
nuintee <nuinteedev@gmail.com>
2023-02-01 06:58:54 +0300

    add: rounded result in calculateDistance features/activities#113

--- src/libs/haversine-distance.ts
+++ src/libs/haversine-distance.ts
@@ -3,11 +3,11 @@ import h from 'haversine-distance'
 type Point = Parameters<typeof h>[number]
 
 const calculateDistance = (a: Point, b: Point, withUnit?: boolean) => {
-  const meter = Math.floor(h(a, b))
+  const meter = h(a, b)
   const mile = meter * 0.000621371 //
   const killo = meter / 1000 //
   const yard = meter * 1.09361
-  const auto = meter >= 1000 ? killo : meter
+  const auto = meter >= 1000 ? Math.round(killo) : Math.round(meter)
 
   const out = {
     meter: `${meter} ${withUnit && 'm'}`,
