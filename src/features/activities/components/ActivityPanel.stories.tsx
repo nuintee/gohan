fe7d3d272791e90fe7d9883fcaaab9683869218f
nuintee <nuinteedev@gmail.com>
2023-02-01 22:31:09 +0300

    update: Activity Panel story dev

--- src/features/activities/components/ActivityPanel.stories.tsx
+++ src/features/activities/components/ActivityPanel.stories.tsx
@@ -5,10 +5,29 @@ import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 
 import ActivityPanel from './ActivityPanel'
+import { queryClient } from '@/libs/tanstack-query'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { SessionProvider } from 'next-auth/react'
+import { RecoilRoot } from 'recoil'
+
+const decorator = (Story, ctx) => {
+  return (
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <SessionProvider>
+          <Story />
+        </SessionProvider>
+      </QueryClientProvider>
+    </RecoilRoot>
+  )
+}
+
+const defaultDecorators = [decorator]
 
 export default {
   title: 'Features/Activities/Panel',
   component: ActivityPanel,
+  decorators: defaultDecorators,
 } as ComponentMeta<typeof ActivityPanel>
 
 //👇 We create a “template” of how args map to rendering
