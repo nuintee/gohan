75f7757af3bd9481b1e94dc4d5e72e508d4fa8ab
nuintee <nuinteedev@gmail.com>
2023-02-02 06:28:47 +0300

    add: useGetDirections refactor/react-query#125

---
+++ src/features/directions/hooks/useGetDirections.tsx
@@ -0,0 +1,43 @@
+import { useQuery } from '@tanstack/react-query'
+import { DirectionsAPI } from '../types/api'
+import useToast from '@/libs/react-toastify'
+
+import { Props } from '../schema/getDirections.schema'
+
+// Env
+import { QUERY_KEY } from '@/features/directions/constants'
+import { BASE_URL } from '@/config/env'
+import axios from '@/libs/axios'
+import useMapBox from '@/features/mapbox/hooks'
+
+const fetcher = ({ start, end }: { start: string; end: string }) => {
+  return axios
+    .get(`${BASE_URL}/api/v1/directions?start=${start}&end=${end}`)
+    .then((res) => res.data)
+}
+
+const useGetDirections = (points: Pick<Props, 'start' | 'end'>) => {
+  const { coords, coordAsString } = useMapBox()
+  const {
+    start = coordAsString(coords as Pick<GeolocationCoordinates, 'latitude' | 'longitude'>),
+    end,
+  } = points
+
+  return useQuery<DirectionsAPI>({
+    queryKey: [QUERY_KEY, { start, end }],
+    queryFn: () => fetcher({ start, end }),
+    enabled: false,
+    onError: (error) => {
+      console.error(error)
+
+      if (error instanceof Error) {
+        useToast.error(error.message)
+      }
+    },
+    onSuccess: (data) => {
+      console.log(data)
+    },
+  })
+}
+
+export default useGetDirections
