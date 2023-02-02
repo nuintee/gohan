94e8143651a02228801a29f7c68a4cffea96b6ac
nuintee <nuinteedev@gmail.com>
2023-02-02 06:33:27 +0300

    add: useClearDirections refactor/react-query#125

---
+++ src/features/directions/hooks/useClearDirections.tsx
@@ -0,0 +1,40 @@
+import { User } from 'next-auth'
+import { useSession } from 'next-auth/react'
+
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import axios from 'axios'
+import useToast from '@/libs/react-toastify'
+
+import { Props } from '../schema/getDirections.schema'
+
+// Env
+import { QUERY_KEY } from '@/features/directions/constants'
+import useMapBox from '@/features/mapbox/hooks'
+
+const useClearDirections = (points: Pick<Props, 'start' | 'end'>) => {
+  const queryClient = useQueryClient()
+  const { coords, coordAsString } = useMapBox()
+
+  const {
+    start = coordAsString(coords as Pick<GeolocationCoordinates, 'latitude' | 'longitude'>),
+    end,
+  } = points
+
+  return useMutation({
+    mutationKey: [QUERY_KEY, { start, end }],
+    mutationFn: async () => {
+      return {} // clear restaurant
+    },
+    onError: (error) => {
+      console.error(error)
+      if (error instanceof Error) {
+        useToast.error(error.message)
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries([QUERY_KEY])
+    },
+  })
+}
+
+export default useClearDirections
