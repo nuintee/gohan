31e0c5d22defc5bb1f1c8ae0fbbecd2269ebb00a
nuintee <nuinteedev@gmail.com>
2023-02-04 00:07:09 +0300

    update: ignore console log in jest refactor/react-query#125

--- src/config/jest/setup.ts
+++ src/config/jest/setup.ts
@@ -1,6 +1,15 @@
 import { handlers } from '@/mocks/handlers'
 import { server } from '@/mocks/server'
 
+global.console = {
+  ...console,
+  log: jest.fn(),
+  debug: jest.fn(),
+  info: jest.fn(),
+  warn: jest.fn(),
+  error: jest.fn(),
+}
+
 beforeAll(() => server.listen())
 afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
--- src/mocks/__test__/index.test.ts
+++ src/mocks/__test__/index.test.ts
@@ -1,66 +1,15 @@
-import places from '@/data/_places.json'
-
-import { server } from '../server'
-
-// handler
-import { handlers } from '../handlers'
-
 // @ts-ignore
 import { fetch as fetchPolyfill } from 'whatwg-fetch'
 
-// data
-
-beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
-
-server.use(...handlers)
-
-describe('health', () => {
-  test('ping: [success]', async () => {
-    const res = await fetchPolyfill('/api/v1/health')
-    const json = await res.json()
-    expect(json).toMatchObject({
-      mock: true,
-      status: 'ok',
-    })
-  })
-})
-
-describe('restaurants', () => {
-  test('places API: [success]', async () => {
-    const res = await fetchPolyfill('/api/v1/restaurants?latitude=20&longitude=20')
-    const json = await res.json()
-    expect(json).toHaveProperty('place_id')
-  })
-  test('details API: [success]', async () => {
-    const res = await fetchPolyfill('/api/v1/restaurants/ChIJ58PFO_yGqkAR1a2dnhgIBiQ')
-    const json = await res.json()
-    expect(json).toHaveProperty('place_id')
-  })
-  test('details API: [error]', async () => {
-    try {
-      const res = await fetchPolyfill('/api/v1/restaurants/INVALID_PLACE_ID')
+describe('Mock APIs', () => {
+  describe('health', () => {
+    test('ping', async () => {
+      const res = await fetchPolyfill('/api/v1/health')
       const json = await res.json()
-      expect(json).toThrowError()
-    } catch (error) {}
-  })
-})
-
-describe('directions', () => {
-  test('Success on request', async () => {
-    const res = await fetchPolyfill('/api/v1/directions?start=20,20&end=20,20', {
-      headers: {
-        'x-place-id': 'ChIJsV5xkfyGqkARsB1A1aMTxZs',
-      },
+      expect(json).toMatchObject({
+        mock: true,
+        status: 'ok',
+      })
     })
-    const json = await res.json()
-    expect(json).toHaveProperty('routes')
-  })
-
-  test('Error on invalid parameters', async () => {
-    const res = await fetchPolyfill('/api/v1/directions')
-    const json = await res.json()
-    expect(json?.code).toBe(500)
   })
 })
