cdafb7eb8587b7e4635225663126776f8a8f2b84
nuintee <nuinteedev@gmail.com>
2023-02-04 07:18:19 +0300

    add: Factory in mocks factory libs/msw-data#133

--- src/mocks/factory/index.ts
+++ src/mocks/factory/index.ts
@@ -1,4 +1,4 @@
-import { factory, primaryKey, nullable } from '@mswjs/data'
+import { factory, primaryKey, nullable, oneOf, manyOf } from '@mswjs/data'
 
 // Prisma
 import { User, Prisma, Activity } from '@prisma/client'
@@ -7,19 +7,20 @@ import { ModelDefinitionValue } from '@mswjs/data/lib/glossary'
 
 type FactoryModel<T> = Record<keyof T, ModelDefinitionValue>
 
-const user: FactoryModel<User> = {
+const user: FactoryModel<User & { activity: [] }> = {
   id: () => randomUUID(),
   name: () => 'DEV_USER',
   email: () => 'devuser@example.com',
   emailVerified: nullable(() => new Date().toISOString()),
   image: () => '',
   registered_at: () => new Date().toISOString(),
+  activity: oneOf('activity'),
 }
 
 const activity: FactoryModel<Activity> = {
   id: () => randomUUID(),
   is_liked: () => false,
-  userId: () => randomUUID(),
+  userId: oneOf('user'),
   place_id: () => '',
   discovered_at: () => new Date().toISOString(),
 }
