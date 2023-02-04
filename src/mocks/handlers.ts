67a3b79517d2fdb80e1f52713d1df4432878913e
nuintee <nuinteedev@gmail.com>
2023-02-04 08:51:06 +0300

    remove: unused loging handler update/hooks#132

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -19,16 +19,6 @@ import { googlePhotosAPIHandler } from './handlers/googlePhotosAPIHandlers'
 const BASE_PATH = '/api/v1'
 
 export const handlers = [
-  rest.post('/login', (req, res, ctx) => {
-    // Persist user's authentication in the session
-    sessionStorage.setItem('is-authenticated', 'true')
-
-    return res(
-      // Respond with a 200 status code
-      ctx.status(200),
-    )
-  }),
-
   rest.get(`${BASE_PATH}/health`, (req, res, ctx) => {
     return res(
       ctx.status(200),
