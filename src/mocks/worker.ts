6fa5ed63a489854b6cfa328cbee95b3f3cc52ebf
nuintee <nuinteedev@gmail.com>
2023-02-04 08:47:27 +0300

    update: onUnhandledRequest print type update/hooks#132

--- src/mocks/worker.ts
+++ src/mocks/worker.ts
@@ -2,10 +2,14 @@ import { MockedRequest, DefaultBodyType } from 'msw'
 
 const IS_BROWSER = typeof window !== 'undefined'
 
+type UnhandledRequestPrint = {
+  warning: () => void
+  error(): void
+}
+
 function onUnhandledRequest(req: MockedRequest<DefaultBodyType>, print: UnhandledRequestPrint) {
   const excludedRoutes = ['/api/v1/restaurants', '/manifest.json', '/logo192.png']
 
-  // check if the req.url.pathname contains excludedRoutes
   const isExcluded = excludedRoutes.some((route) => req.url.pathname.includes(route))
 
   if (isExcluded) {
