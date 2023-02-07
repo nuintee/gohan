3ede03341340dc602ab5459a69148d81f6a70ae8
nuintee <nuinteedev@gmail.com>
2023-02-07 03:07:11 +0300

    hooks/directions#136

--- src/features/directions/hooks/useGetDirections.tsx
+++ src/features/directions/hooks/useGetDirections.tsx
@@ -1,57 +1,15 @@
-import { useQuery } from '@tanstack/react-query'
-import { DirectionsAPI } from '../types/api'
 import useToast from '@/libs/react-toastify'
 
-import { Coordinates, Props } from '../schema/getDirections.schema'
+import { Coordinates } from '../schema/getDirections.schema'
 
-// Env
-import { QUERY_KEY } from '@/features/directions/constants'
-import { BASE_URL } from '@/config/env'
-import axios from '@/libs/axios'
 import useMapBox from '@/features/mapbox/hooks'
 import { trpc } from '@/libs/trpc'
-
-const fetcher = ({ start, end }: { start: string; end: string }) => {
-  return axios
-    .get(`${BASE_URL}/api/v1/directions?start=${start}&end=${end}`)
-    .then((res) => res.data)
-}
-
-// const useGetDirections = (points: Partial<Pick<Props, 'start' | 'end'>>) => {
-//   const { coords, coordAsString } = useMapBox()
-//   const {
-//     start = coordAsString(coords as Pick<GeolocationCoordinates, 'latitude' | 'longitude'>),
-//     end,
-//   } = points
-
-//   const isGPSAvailable = coords.latitude && coords.longitude
-
-//   return useQuery<DirectionsAPI>({
-//     queryKey: [QUERY_KEY, { start, end }],
-//     queryFn: () => {
-//       if (!isGPSAvailable) throw Error('Please allow user location tracking')
-//       return fetcher({ start, end })
-//     },
-//     enabled: false,
-//     ...(!isGPSAvailable && { retry: 0 }),
-//     onError: (error) => {
-//       console.error(error)
-
-//       if (error instanceof Error) {
-//         useToast.error(error.message)
-//       }
-//     },
-//     onSuccess: (data) => {
-//       console.log(data)
-//     },
-//   })
-// }
 
 const useGetDirections = (props: Coordinates[]) => {
   const { coords } = useMapBox()
   const [a, b = coords] = props
 
-  return trpc.getDirections.useQuery([a, b], {
+  return trpc.getDirections.useQuery([a, b as Required<typeof b>], {
     enabled: false,
     onError: (error) => {
       console.error(error)
--- src/features/directions/schema/getDirections.schema.ts
+++ src/features/directions/schema/getDirections.schema.ts
@@ -18,26 +18,4 @@ export const CoordinatesSchema = z.object({
   ),
 })
 
-export const Schema = z.object({
-  profileType: z.optional(
-    z.string().refine((v) => ['walking', 'driving-traffic', 'driving', 'cycling'].includes(v)),
-  ),
-  start: z.string().refine((val) => {
-    const [latitude, longitude] = val.split(',')
-    const validLongitude = Number(longitude) >= -180 && Number(longitude) <= 180
-    const validLatitude = Number(latitude) >= -90 && Number(latitude) <= 90
-    return validLatitude && validLongitude
-  }),
-  end: z.string().refine((val) => {
-    const [latitude, longitude] = val.split(',')
-    const validLongitude = Number(longitude) >= -180 && Number(longitude) <= 180
-    const validLatitude = Number(latitude) >= -90 && Number(latitude) <= 90
-    return validLatitude && validLongitude
-  }),
-  ...(IS_DEVMODE && {
-    place_id: z.optional(z.string()),
-  }),
-})
-
-export type Props = z.infer<typeof Schema>
 export type Coordinates = z.infer<typeof CoordinatesSchema>
