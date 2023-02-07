ee7636cbf8708e08f8394e79a07e0c0cf51dc059
nuintee <nuinteedev@gmail.com>
2023-02-07 08:06:58 +0300

    add: usePatchActivity hooks/activities#138

--- src/features/activities/hooks/useGetActivity.tsx
+++ src/features/activities/hooks/useGetActivity.tsx
@@ -4,6 +4,7 @@ import { trpc } from '@/libs/trpc'
 const useGetActivity = (props: Parameters<typeof trpc.getActivity.useQuery>[0]) => {
   return trpc.getActivity.useQuery(props, {
     onError: (error) => {
+      console.error(error)
       if (error instanceof Error) {
         useToast.error(error.message)
       }
--- src/features/activities/hooks/useGetUserActivities.tsx
+++ src/features/activities/hooks/useGetUserActivities.tsx
@@ -4,6 +4,7 @@ import { trpc } from '@/libs/trpc'
 const useGetUserActivities = (props: Parameters<typeof trpc.getUserActivities.useQuery>[0]) => {
   return trpc.getUserActivities.useQuery(props, {
     onError: (error) => {
+      console.error(error)
       if (error instanceof Error) {
         useToast.error(error.message)
       }
