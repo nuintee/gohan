c5149d4070d96621f5c10dfcd7a52843907ca3a2
nuintee <nuinteedev@gmail.com>
2023-02-07 09:03:08 +0300

    removed: mock handlers except google apis refactor/remove-unused-code#144

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -4,19 +4,7 @@ import { googlePhotosAPIHandler } from './handlers/googlePhotosAPIHandlers'
 import { googleDetailsAPIHandlers } from './handlers/googleDetailsAPIHandlers'
 import { googlePlacesHandlers } from './handlers/googlePlacesHandlers'
 
-const BASE_PATH = '/api/trpc'
-
 export const handlers = [
-  rest.get(`${BASE_PATH}/health`, (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json({
-        status: 'ok',
-        mock: true,
-      }),
-    )
-  }),
-
   rest.get(`https://maps.googleapis.com/maps/api/place/photo`, googlePhotosAPIHandler),
   rest.get(`https://maps.googleapis.com/maps/api/place/details/json`, googleDetailsAPIHandlers),
   rest.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json`, googlePlacesHandlers),
