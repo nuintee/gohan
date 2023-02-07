7d1b4a75ef0d472322799e937e729f0466048259
nuintee <nuinteedev@gmail.com>
2023-02-07 08:09:21 +0300

    update: useDelete Activity hooks/activities#138

--- src/features/activities/hooks/useDeleteActivity.tsx
+++ src/features/activities/hooks/useDeleteActivity.tsx
@@ -4,6 +4,7 @@ import { trpc } from '@/libs/trpc'
 const useDeleteActivity = () => {
   return trpc.deleteActivity.useMutation({
     onError: (error) => {
+      console.error(error)
       if (error instanceof Error) {
         useToast.error(error.message)
       }
