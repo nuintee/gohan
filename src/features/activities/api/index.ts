74952e1fd7baa8c512d4f32ee1003dee87f73fb2
nuintee <nuinteedev@gmail.com>
2023-02-07 09:14:51 +0300

    update: activities schema rename refactor/remove-unused-code#144

--- src/features/activities/api/index.ts
+++ src/features/activities/api/index.ts
@@ -4,7 +4,7 @@ import { z } from 'zod'
 import { IS_DEVMODE, IS_PRODMODE } from '@/config/env'
 import { randomUUID } from 'crypto'
 
-import { addActivitySchema, updateActivitySchema } from '../schemas/index.schema'
+import { AddActivitySchema, UpdateActivitySchema } from '../schemas/index.schema'
 
 export const getActivity = procedure
   .input(
@@ -45,7 +45,7 @@ export const deleteActivity = procedure
     }
   })
 
-export const updateActivity = procedure.input(updateActivitySchema).mutation(async ({ input }) => {
+export const updateActivity = procedure.input(UpdateActivitySchema).mutation(async ({ input }) => {
   if (IS_DEVMODE) {
     // mock
     const data = await prisma.activity.update({
@@ -62,7 +62,7 @@ export const updateActivity = procedure.input(updateActivitySchema).mutation(asy
   }
 })
 
-export const addActivity = procedure.input(addActivitySchema).mutation(async ({ input }) => {
+export const addActivity = procedure.input(AddActivitySchema).mutation(async ({ input }) => {
   const { activityId = randomUUID(), userId, place_id = '', is_liked = false } = input
 
   if (IS_DEVMODE) {
--- src/features/activities/schemas/index.schema.ts
+++ src/features/activities/schemas/index.schema.ts
@@ -1,18 +1,18 @@
 import { z } from 'zod'
 
-export const addActivitySchema = z.object({
+export const AddActivitySchema = z.object({
   activityId: z.optional(z.string().uuid()),
   userId: z.string().uuid(),
   place_id: z.optional(z.string()),
   is_liked: z.optional(z.boolean()),
 })
 
-export const updateActivitySchema = z.object({
+export const UpdateActivitySchema = z.object({
   activityId: z.string(),
   payload: z.object({
     is_liked: z.boolean(),
   }),
 })
 
-export type AddActivityProps = z.infer<typeof addActivitySchema>
-export type UpdateActivityProps = z.infer<typeof updateActivitySchema>
+export type AddActivityProps = z.infer<typeof AddActivitySchema>
+export type UpdateActivityProps = z.infer<typeof UpdateActivitySchema>
