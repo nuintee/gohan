a0dfff81fe112eb0a8bd4159e6a4065d85a3c42e
nuintee <nuinteedev@gmail.com>
2023-02-07 08:03:08 +0300

    add: useGetActivity hooks/activities#138

--- src/features/activities/hooks/useGetActivity.tsx
+++ src/features/activities/hooks/useGetActivity.tsx
@@ -1,7 +1,14 @@
+import useToast from '@/libs/react-toastify'
 import { trpc } from '@/libs/trpc'
 
 const useGetActivity = (props: Parameters<typeof trpc.getActivity.useQuery>[0]) => {
-  return trpc.getActivity.useQuery(props)
+  return trpc.getActivity.useQuery(props, {
+    onError: (error) => {
+      if (error instanceof Error) {
+        useToast.error(error.message)
+      }
+    },
+  })
 }
 
 export default useGetActivity
