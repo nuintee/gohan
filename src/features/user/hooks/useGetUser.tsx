0593520b8b988ac96166554be7f75eafa515894c
nuintee <nuinteedev@gmail.com>
2023-02-07 08:44:39 +0300

    update: useGetUser hooks/user#142

--- src/features/user/hooks/useGetUser.tsx
+++ src/features/user/hooks/useGetUser.tsx
@@ -1,37 +1,17 @@
-import { User } from 'next-auth'
-import { useSession } from 'next-auth/react'
-
-import { useQuery } from '@tanstack/react-query'
-import axios from 'axios'
 import useToast from '@/libs/react-toastify'
+import { trpc } from '@/libs/trpc'
 
-// Env
-import { QUERY_KEY } from '../constants'
-import { BASE_URL } from '@/config/env'
-
-const fetcher = (userId?: string) => {
-  if (!userId) throw Error('Unauthed Request', { cause: 'User Must be Authed' })
-
-  return axios.get(`${BASE_URL}/api/v1/user/${userId}`).then((res) => res.data)
-}
-
-type Props = {
-  userId: string
-}
-
-const useGetUser = (props?: Props) => {
-  const { status, data: session } = useSession()
-  const userId = props?.userId || session?.user.id
-
-  return useQuery({
-    queryKey: [QUERY_KEY, { user: userId }],
-    queryFn: () => fetcher(userId),
-    enabled: status === 'authenticated',
+const useGetUser = (props: Parameters<typeof trpc.getUser.useQuery>[0]) => {
+  return trpc.getUser.useQuery(props, {
     onError: (error) => {
       console.error(error)
+
       if (error instanceof Error) {
         useToast.error(error.message)
       }
+    },
+    onSuccess: (data) => {
+      console.log(data)
     },
   })
 }
