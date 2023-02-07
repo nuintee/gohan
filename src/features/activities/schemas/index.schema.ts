7d91f15435b77c9288723b95d93a5c20cd8fe40f
nuintee <nuinteedev@gmail.com>
2023-02-07 09:11:17 +0300

    update: addActivity and updateActivity schema extracted refactor/remove-unused-code#144

--- src/features/activities/schemas/index.schema.ts
+++ src/features/activities/schemas/index.schema.ts
@@ -1,13 +1,18 @@
 import { z } from 'zod'
 
 export const addActivitySchema = z.object({
-  id: z.optional(z.string().uuid()),
-  userId: z.string(),
+  activityId: z.optional(z.string().uuid()),
+  userId: z.string().uuid(),
   place_id: z.optional(z.string()),
   is_liked: z.optional(z.boolean()),
 })
 
-export const updateActivitySchema = addActivitySchema.partial()
+export const updateActivitySchema = z.object({
+  activityId: z.string(),
+  payload: z.object({
+    is_liked: z.boolean(),
+  }),
+})
 
 export type AddActivityProps = z.infer<typeof addActivitySchema>
 export type UpdateActivityProps = z.infer<typeof updateActivitySchema>
