a45172863722f27d58140957fac2951354728975
nuintee <nuinteedev@gmail.com>
2023-02-09 12:14:48 +0300

    add: prod api api/direcitons#149

--- src/features/directions/api/index.ts
+++ src/features/directions/api/index.ts
@@ -3,10 +3,12 @@ import prisma from '@/libs/prisma'
 import { z } from 'zod'
 
 // Env
-import { IS_DEVMODE, IS_PRODMODE } from '@/config/env'
+import { IS_DEVMODE, IS_PRODMODE, MAPBOX_PUBLIC_TOKEN } from '@/config/env'
 
 // Schema
 import { CoordinatesSchema } from '../schema/coordinates.schema'
+import { sleep } from '@/utils/sleep'
+import axios from 'axios'
 
 export const getDirections = procedure
   .input(z.array(CoordinatesSchema).max(2))
@@ -15,15 +17,27 @@ export const getDirections = procedure
 
     if (IS_DEVMODE) {
       // mock as msw
+      sleep(1000)
+
       return {
         a,
         b,
       }
     } else if (IS_PRODMODE) {
-      return {
-        a,
-        b,
-      }
+      const url = new URL(
+        `https://api.mapbox.com/directions/v5/mapbox/walking/${a.latitude},${a.longitude};${b.latitude},${b.longitude}`,
+      )
+      url.searchParams.append('alternatives', 'true')
+      url.searchParams.append('continue_straight', 'true')
+      url.searchParams.append('geometries', 'geojson')
+      url.searchParams.append('language', 'en')
+      url.searchParams.append('overview', 'simplified')
+      url.searchParams.append('steps', 'true')
+      url.searchParams.append('access_token', MAPBOX_PUBLIC_TOKEN)
+
+      const { data } = await axios.get(url.toString())
+
+      return data
     } else {
       // test
       return {
