28e76427c055f597f57e94faa54251ace4520f55
nuintee <nuinteedev@gmail.com>
2023-02-09 14:08:34 +0300

    remove: directions feature directory api/direcitons#149

--- src/features/directions/schema/directions.schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod'
-import { CoordinatesSchema } from './coordinates.schema'
-
-export const DirectionsInputSchema = z.object({
-  start: CoordinatesSchema,
-  destination: CoordinatesSchema,
-})
-
-export type DirectionsInput = z.infer<typeof DirectionsInputSchema>
--- src/pages/_index.tsx
+++ src/pages/_index.tsx
@@ -12,13 +12,11 @@ import AcitvityButton from '@/features/activities/components/ActivityButton'
 import UserSettingsModal from '@/features/user/components/UserSettingsModal'
 import ActivityPanel from '@/features/activities/components/ActivityPanel'
 import RestaurantDiscoveredModal from '@/features/restaurants/components/RestaurantDiscoveredModal'
-import useGetDirections from '@/features/directions/hooks/useGetDirections'
 import useExperimentalRestaurants from '@/features/restaurants/hooks/useRestaurants'
 import RestaurantCard from '@/features/restaurants/components/RestaurantCard'
 import { useCallback, useState } from 'react'
 import { QueryClient, useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { trpc } from '@/libs/trpc'
-import useGeoJSON from '@/features/directions/hooks/useGeoJSON'
 import { ActivityResolved } from '@/features/activities/types'
 
 const Index = () => {
@@ -26,62 +24,13 @@ const Index = () => {
   const { isOpen, getPayload, close, open } = useModals()
   const { coords } = useMapBox()
 
-  // Directions
-  const createGeoJSON = useGeoJSON()
-  const INITIAL_DIRECTIONS = { source: {}, layer: {}, data: {} }
-  const [directions, setDirections] = useState(INITIAL_DIRECTIONS)
-
   const restaurants = useExperimentalRestaurants({
     latitude: coords.latitude,
     longitude: coords.longitude,
   })
 
-  const getDirections = useGetDirections(
-    {
-      start: {
-        latitude: coords.latitude,
-        longitude: coords.longitude,
-      },
-      destination: {
-        latitude: 0,
-        longitude: 0,
-      },
-    },
-    (data) => {
-      setDirections((prev) => ({
-        ...prev,
-        ...createGeoJSON({
-          coordinates: data.routes[0].geometry.coordinates,
-        }),
-      }))
-    },
-  )
-
-  const isNavigating = () => {
-    if (!directions) return false
-
-    const isSource = Object.keys(directions.source).length > 0
-    const isLayer = Object.keys(directions.layer).length > 0
-    return isSource && isLayer
-  }
-
-  const handleNavigate = (activity: ActivityResolved) => {
-    if (!activity) return
-
-    if (isNavigating() && directions.data?.place_id === activity?.place_id) {
-      setDirections(INITIAL_DIRECTIONS)
-    } else {
-      setDirections((prev) => ({ ...prev, data: activity }))
-      getDirections.refetch()
-    }
-  }
-
   const handleClick = () => {
-    if (isNavigating()) {
-      setDirections(INITIAL_DIRECTIONS)
-    } else {
-      restaurants.refetch()
-    }
+    restaurants.refetch()
   }
 
   return (
@@ -91,18 +40,10 @@ const Index = () => {
           <User />
           <AcitvityButton />
         </section>
-        <MapBox geoJSON={{ ...directions.source, ...directions.layer }} />
+        <MapBox />
         <section className='absolute bottom-0 left-0 z-[1] w-full flex items-center justify-center p-4 flex-col gap-4'>
-          {isNavigating() && (
-            <RestaurantCard
-              data={directions.data}
-              compact
-              onClick={() => open('restaurantdiscovered', directions.data)}
-            />
-          )}
           <GohanButton
             onClick={() => handleClick()}
-            isNavigating={isNavigating()}
             isLoading={restaurants.isFetching}
             disabled={restaurants.isFetching}
           />
@@ -114,12 +55,6 @@ const Index = () => {
         isOpen={isOpen('restaurantdiscovered')}
         onClose={() => close('restaurantdiscovered')}
         data={getPayload('restaurantdiscovered')}
-        isLoading={getDirections.isFetching}
-        onNavigate={handleNavigate}
-        isNavigating={
-          isNavigating() &&
-          getPayload('restaurantdiscovered')?.place_id === directions.data?.place_id
-        }
       />
       <UserAuthConsentDialog />
       <UserSettingsModal />
