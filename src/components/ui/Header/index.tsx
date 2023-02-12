494ebeff0da730c4572d9c26b49d341ee75387ca
nuintee <nuinteedev@gmail.com>
2023-02-12 18:49:45 +0300

    add: Header components/header#153

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -6,7 +6,10 @@ const Header = () => {
   return (
     <div className='flex gap-4 bg-gh-brown justify-between px-[10%]'>
       <Brand margin={true} />
-      <Link href='/library' className={`ml-auto p-2 items-center flex border-b-2  text-gh-white`}>
+      <Link
+        href='/library'
+        className={`ml-auto p-2 items-center flex border-b-2 border-transparent hover:text-white active:text-white active:border-gh-orange hover:border-gh-orange  text-gh-white`}
+      >
         Library
       </Link>
       <div className='flex items-center'>
