d37af95608a915a287b9236930b57dc676824889
nuintee <nuinteedev@gmail.com>
2023-02-13 22:11:46 +0300

    add: placeholdre color pages/details#157

--- src/pages/details/[place_id].tsx
+++ src/pages/details/[place_id].tsx
@@ -70,19 +70,23 @@ const DetailsPage = ({ data }: { data: ActivityResolved }) => {
             <Input placeholder='メモ' />
           </section>
           <section className='flex items-center justify-between gap-4  mb-14'>
-            <DescriptiveChip title='超高級' description={data.price_level} icon={<Price />} />
+            <DescriptiveChip
+              title='超高級'
+              description={data.price_level}
+              icon={<Price fill={colors['gh-red']} />}
+            />
             <DescriptiveChip
               title='営業中'
               description={`営業時間: ${
                 data.opening_hours?.periods && data.opening_hours?.periods[0]
               }`}
-              icon={<Clock />}
+              icon={<Clock fill={colors['gh-green']} />}
             />
             {data.user_ratings_total > 0 && (
               <DescriptiveChip
                 title={`悪い評価`}
                 description={`Googleでの評価は${data.rating}です。`}
-                icon={<Star />}
+                icon={<Star fill={colors['gh-red']} />}
               />
             )}
           </section>
