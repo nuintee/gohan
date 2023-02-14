1a6d288a8febcda4d1d5dbbb214c0dd69141f920
nuintee <nuinteedev@gmail.com>
2023-02-14 18:04:46 +0300

    add: DetailsSection in features layout pages/details#157

--- src/features/details/components/ReviewModal.tsx
+++ src/features/details/components/ReviewModal.tsx
@@ -33,6 +33,7 @@ const ReviewModal = ({ isOpen, onClose, data }: Props) => {
                 style={{
                   backgroundColor: COLORS[v],
                 }}
+                key={v}
               >
                 {v}
               </button>
