1cb1ac4781e432cad7a0b9d53aacae2b8eff80b8
nuintee <nuinteedev@gmail.com>
2023-02-14 23:43:01 +0300

    add: active border style when on library page in Header link pages/library#159

---
+++ src/pages/library.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/ui/Header'
+
+const LibraryPage = () => {
+  return (
+    <div className='flex flex-col gap-4 h-full w-full'>
+      <Header />
+    </div>
+  )
+}
+
+export default LibraryPage
