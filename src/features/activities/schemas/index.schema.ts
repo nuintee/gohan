21f9ce9e335828bbc7af1ef163ff1f82b93ce707
nuintee <nuinteedev@gmail.com>
2023-02-16 19:37:56 +0300

    add: ReviewStatusEnum activities/prisma-schema#161

--- src/features/activities/schemas/index.schema.ts
+++ src/features/activities/schemas/index.schema.ts
@@ -2,22 +2,21 @@ import { z } from 'zod'
 
 import { ReviewStatus } from '@prisma/client'
 
-const REVIEW_STATUSES: ReviewStatus[] = ['BAD', 'GOOD', 'NEW', 'OK']
+const REVIEW_STATUS_ENUM = z.nativeEnum(ReviewStatus)
 
 export const AddActivitySchema = z.object({
   activityId: z.optional(z.string().uuid()),
   userId: z.string().uuid(),
   place_id: z.optional(z.string()),
   memo: z.optional(z.string()),
+  reviewStatus: REVIEW_STATUS_ENUM,
 })
 
 export const UpdateActivitySchema = z.object({
   activityId: z.string(),
   payload: z.object({
     memo: z.optional(z.string()),
-    reviewStatus: z.string().refine((v) => {
-      return REVIEW_STATUSES.includes(v)
-    }),
+    reviewStatus: REVIEW_STATUS_ENUM,
   }),
 })
 
