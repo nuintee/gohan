28f8e10d3ca46f880995c7c494aee5a8028caf89
nuintee <nuinteedev@gmail.com>
2023-02-17 00:07:10 +0300

    add: notFound in getServerSideProps activities/prisma-schema#161

--- src/features/activities/hooks/useGetActivity.tsx
+++ src/features/activities/hooks/useGetActivity.tsx
@@ -1,5 +1,6 @@
 import useToast from '@/libs/react-toastify'
 import { trpc } from '@/libs/trpc'
+import { useRouter } from 'next/router'
 
 const useGetActivity = (props: Parameters<typeof trpc.getActivity.useQuery>[0]) => {
   return trpc.getActivity.useQuery(props, {
