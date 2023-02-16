b675cc4e86f8c06a6c45f9fc5fa2fa29604be053
nuintee <nuinteedev@gmail.com>
2023-02-16 17:30:24 +0300

    add: ReviewStatus type from prisma client activities/prisma-schema#161

--- src/features/activities/schemas/index.schema.ts
+++ src/features/activities/schemas/index.schema.ts
@@ -1,16 +1,23 @@
 import { z } from 'zod'
 
+import { ReviewStatus } from '@prisma/client'
+
+const REVIEW_STATUSES: ReviewStatus[] = ['BAD', 'GOOD', 'NEW', 'OK']
+
 export const AddActivitySchema = z.object({
   activityId: z.optional(z.string().uuid()),
   userId: z.string().uuid(),
   place_id: z.optional(z.string()),
-  is_liked: z.optional(z.boolean()),
+  memo: z.optional(z.string()),
 })
 
 export const UpdateActivitySchema = z.object({
   activityId: z.string(),
   payload: z.object({
-    is_liked: z.boolean(),
+    memo: z.optional(z.string()),
+    reviewStatus: z.string().refine((v) => {
+      return REVIEW_STATUSES.includes(v)
+    }),
   }),
 })
 
