03f8d53b576c6daf10e872771adb88ef3eff3014
nuintee <nuinteedev@gmail.com>
2023-02-18 13:49:39 +0300

    add: Error FallbackPage activities/prisma-schema#161

--- src/components/fallback/ErrorFallback/index.tsx
+++ src/components/fallback/ErrorFallback/index.tsx
@@ -1,11 +1,18 @@
+import { Button } from '@/components/ui'
 import { TRPCClientErrorBase } from '@trpc/client'
 import { DefaultErrorShape } from '@trpc/server'
+import { useRouter } from 'next/router'
 
 const ErrorFallBack = ({ error }: { error: TRPCClientErrorBase<DefaultErrorShape> | Error }) => {
+  const router = useRouter()
+
   return (
-    <div className='h-screen w-screen'>
+    <div className='h-screen w-screen flex flex-col gap-4 items-center justify-center'>
       <h1>エラー</h1>
       <p>{error.message}</p>
+      <div>
+        <Button text='ホームへ戻る' onClick={() => router.push('/')} />
+      </div>
     </div>
   )
 }
