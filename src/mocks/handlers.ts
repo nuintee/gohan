7621795d23963b0c7a626d731ace3c7737be1e1f
nuintee <nuinteedev@gmail.com>
2023-02-18 12:29:39 +0300

    add: getActivity API mock server activities/prisma-schema#161

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,9 +3,17 @@ import { rest } from 'msw'
 import { googlePhotosAPIHandler } from './handlers/googlePhotosAPIHandlers'
 import { googleDetailsAPIHandlers } from './handlers/googleDetailsAPIHandlers'
 import { googlePlacesHandlers } from './handlers/googlePlacesHandlers'
+import { BASE_URL } from '@/config/env'
 
 export const handlers = [
   rest.get(`https://maps.googleapis.com/maps/api/place/photo`, googlePhotosAPIHandler),
   rest.get(`https://maps.googleapis.com/maps/api/place/details/json`, googleDetailsAPIHandlers),
   rest.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json`, googlePlacesHandlers),
+  rest.get(`${BASE_URL}/api/trpc/getActivity`, (req, res, ctx) => {
+    const input = req.url.searchParams.get('input')
+    const decoded = decodeURI(input)
+    const parsed = JSON.parse(decoded)
+    console.log(parsed)
+    return res(ctx.json({}))
+  }),
 ]
