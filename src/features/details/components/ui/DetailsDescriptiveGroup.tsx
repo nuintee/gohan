b928f2d9052840d8f41dc14e47592250d14881ca
nuintee <nuinteedev@gmail.com>
2023-02-21 02:05:30 +0300

    add: conditional descriptiveChip components/descriptive-chip#195

--- src/features/details/components/ui/DetailsDescriptiveGroup.tsx
+++ src/features/details/components/ui/DetailsDescriptiveGroup.tsx
@@ -13,18 +13,24 @@ const DetailsDescriptiveGroup = ({
 }) => {
   return (
     <section className='flex items-center justify-between gap-4 my-14'>
-      <DescriptiveChip
-        title={'高級'}
-        description={''}
-        icon={<Price fill={colors['gh-red']} />}
-        isLoading={isLoading}
-      />
-      <DescriptiveChip
-        title='営業中'
-        description={`営業時間: ${data.opening_hours?.periods && data?.opening_hours?.periods[0]}`}
-        icon={<Clock fill={colors['gh-green']} />}
-        isLoading={isLoading}
-      />
+      {data.price_level && (
+        <DescriptiveChip
+          title={'高級'}
+          description={''}
+          icon={<Price fill={colors['gh-red']} />}
+          isLoading={isLoading}
+        />
+      )}
+      {data.opening_hours && (
+        <DescriptiveChip
+          title='営業中'
+          description={`営業時間: ${
+            data.opening_hours?.periods && data?.opening_hours?.periods[0]
+          }`}
+          icon={<Clock fill={colors['gh-green']} />}
+          isLoading={isLoading}
+        />
+      )}
       {data?.user_ratings_total && data?.user_ratings_total > 0 && (
         <DescriptiveChip
           title={`悪い評価`}
