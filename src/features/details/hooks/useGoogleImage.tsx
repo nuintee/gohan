4687f1a0f87d908259bf37843278af493d11121f
nuintee <nuinteedev@gmail.com>
2023-02-22 03:04:35 +0300

    add: random pastel color placehlder image fallback in usePlaceImage dev

--- src/features/details/hooks/useGoogleImage.tsx
+++ src/features/details/hooks/useGoogleImage.tsx
@@ -1,10 +1,22 @@
+import { colors } from '@/config/colors'
 import { GCP_API_KEY } from '@/config/env'
 import { PhotosEntity, ResultsEntity } from '@/features/restaurants/types'
 
+function _generatePastelColor() {
+  let R = Math.floor(Math.random() * 127 + 127)
+  let G = Math.floor(Math.random() * 127 + 127)
+  let B = Math.floor(Math.random() * 127 + 127)
+
+  let rgb = (R << 16) + (G << 8) + B
+  return `${rgb.toString(16)}`
+}
+
 const usePlacePhotos = (photos: ResultsEntity['photos']) => {
+  const RANDOM_COLOR = _generatePastelColor()
+
   if (!photos)
     return {
-      url: 'https://via.placeholder.com/600x400/FFF/png?text',
+      url: `https://via.placeholder.com/600x400/${RANDOM_COLOR}/FFF.png?text=NO_IMAGE`,
       width: 400,
       height: 400,
       html_attributions: [],
