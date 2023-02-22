cd467a4fbddf04e853a245dc7b97dd5c64394a2d
nuintee <nuinteedev@gmail.com>
2023-02-23 02:41:12 +0300

    add: MapBoxChip with lcoate on map button pages/details#203

--- src/features/details/components/ui/DetailsSectionGroup.tsx
+++ src/features/details/components/ui/DetailsSectionGroup.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/ui'
 import { ActivityResolved } from '@/features/activities/types'
 import MapBoxChip from '@/features/mapbox/components/MapBoxChip'
+import Pin from '@/features/mapbox/components/MarkerPin'
 import useMapBox from '@/features/mapbox/hooks'
 import { useRouter } from 'next/router'
 import DetailsSection from '../../layouts/DetailsSection'
@@ -12,27 +13,26 @@ const DetailsSectionGroup = ({
   data: ActivityResolved
   isLoading: boolean
 }) => {
-  const router = useRouter()
-
-  const handleShowMap = () => {
-    router.push('/library')
-  }
+  const { onActivityClicked, mapbox } = useMapBox()
 
   return (
     <>
-      <DetailsSection
-        margin='5rem'
-        main='ロケーション'
-        sub={data?.vicinity}
-        isLoading={isLoading}
-        actionLabel={'ライブラリマップで表示'}
-        actionCallback={handleShowMap}
-      >
-        <div className='flex-1 aspect-video w-full'>
+      <DetailsSection margin='5rem' main='ロケーション' sub={data?.vicinity} isLoading={isLoading}>
+        <div className='flex-1 aspect-video w-full relative'>
           <MapBoxChip
             latitude={data?.geometry?.location.lat}
             longitude={data?.geometry?.location.lng}
-          />
+          >
+            <Pin
+              latitude={data?.geometry?.location.lat}
+              longitude={data?.geometry?.location.lng}
+              data={data}
+              focused={false}
+            />
+          </MapBoxChip>
+          <div className='absolute right-4 top-4'>
+            <Button text='マップ上で表示' onClick={() => onActivityClicked(data)} />
+          </div>
         </div>
       </DetailsSection>
       {data?.user_ratings_total && data?.user_ratings_total > 0 && (
--- src/features/mapbox/components/MapBoxChip.tsx
+++ src/features/mapbox/components/MapBoxChip.tsx
@@ -1,7 +1,18 @@
 import { MAPBOX_PUBLIC_TOKEN } from '@/config/env'
-import { Map, Marker, ViewState } from 'react-map-gl'
+import { ReactElement } from 'react'
+import { GeolocateControl, Map, Marker, ViewState } from 'react-map-gl'
+import useMapBox from '../hooks'
+import Pin from './MarkerPin'
 
-const MapBoxCore = ({ latitude = 90, longitude = 90, zoom = 15, ...rest }: Partial<ViewState>) => {
+const MapBoxCore = ({
+  latitude = 90,
+  longitude = 90,
+  zoom = 15,
+  children,
+  ...rest
+}: Partial<ViewState> & { children?: ReactElement }) => {
+  const { setMapBoxRef } = useMapBox()
+
   return (
     <Map
       initialViewState={{
@@ -10,6 +21,7 @@ const MapBoxCore = ({ latitude = 90, longitude = 90, zoom = 15, ...rest }: Parti
         zoom,
         ...rest,
       }}
+      ref={setMapBoxRef}
       renderWorldCopies={false}
       pitchWithRotate={false}
       dragPan={false}
@@ -17,7 +29,20 @@ const MapBoxCore = ({ latitude = 90, longitude = 90, zoom = 15, ...rest }: Parti
       mapboxAccessToken={MAPBOX_PUBLIC_TOKEN}
       mapStyle='mapbox://styles/nuinteedev/cldaz8llv002c01mgt375yl40'
     >
-      <Marker latitude={latitude} longitude={longitude} />
+      <GeolocateControl
+        showAccuracyCircle
+        trackUserLocation
+        showUserLocation
+        showUserHeading
+        position='bottom-right'
+        onError={() => {}}
+        onGeolocate={() => {}}
+        style={{
+          padding: '0.5rem',
+          borderRadius: '100%',
+        }}
+      />
+      {children}
     </Map>
   )
 }
