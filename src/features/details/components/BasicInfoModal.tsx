5096e59743c281862be39bd4e8b7679ccd498d83
nuintee <nuinteedev@gmail.com>
2023-02-24 20:40:28 +0300

    remove: legacy DetailsSummray component in BasicInfoModal component/details-basic-info#213

--- src/features/details/components/BasicInfoModal.tsx
+++ src/features/details/components/BasicInfoModal.tsx
@@ -1,6 +1,4 @@
-import { Chevron } from '@/components/icons'
-import { Button, PanelHeader } from '@/components/ui'
-import DetailsSummary from '@/components/ui/DetailsSummary'
+import { Button, PanelHeader, DetailsSummary } from '@/components/ui'
 import { ResultsEntity } from '@/features/restaurants/types'
 import ModalLayout from '@/layouts/ModalLayout'
 import { BASIC_INFO_KEYS } from '../constants'
@@ -11,31 +9,6 @@ type Props = {
   onClose?: React.MouseEventHandler<HTMLButtonElement>
   data: ResultsEntity
 }
-
-// const DetailsSummray = ({
-//   ignored = false,
-//   summaryTitle,
-//   summaryValue,
-//   children,
-// }: {
-//   ignored?: boolean
-//   summaryTitle?: string
-//   summaryValue?: string
-//   children: JSX.Element
-// }) => {
-//   return (
-//     <details className={`w-full group ${ignored && 'pointer-events-none'}`}>
-//       <summary className='flex items-center justify-between  cursor-pointer'>
-//         <div className='flex items-center gap-2'>
-//           {!ignored && <Chevron overrideClassName='rotate-90 group-open:-rotate-90' />}
-//           <p>{summaryTitle}</p>
-//         </div>
-//         <h2>{summaryValue}</h2>
-//       </summary>
-//       <div className='py-2 divide-y flex flex-col gap-1'>{children}</div>
-//     </details>
-//   )
-// }
 
 const BasicInfoModal = (props: Props) => {
   const { isOpen, onClose, data } = props
