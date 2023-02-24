5fd3383a2e360e66367131cbadba0b7cb134f511
nuintee <nuinteedev@gmail.com>
2023-02-24 04:45:31 +0300

    add: keyFeatures in different file pages/details#203

---
+++ src/features/details/constants/keyFeatures.ts
@@ -0,0 +1,11 @@
+export const KEY_FEATURES = [
+  'delivery',
+  'dine_in',
+  'reservable',
+  'serves_breakfast',
+  'serves_brunch',
+  'serves_dinner',
+  'serves_lunch',
+  'serves_vegetarian_food',
+  'wheelchair_accessible_entrance',
+] as const
--- src/features/details/hooks/useKeyFeatures.tsx
+++ src/features/details/hooks/useKeyFeatures.tsx
@@ -1,7 +1,14 @@
 import useGetActivity from '@/features/activities/hooks/useGetActivity'
 import { ActivityResolved } from '@/features/activities/types'
 
+// consts
+import { KEY_FEATURES } from '../constants/keyFeatures'
+
 function useKeyFeatures<T extends ReturnType<typeof useGetActivity>['data']>(data: T) {
+  const FEATURES = Object.keys(data).filter((v) => KEY_FEATURES.includes(v))
+
+  console.log(FEATURES)
+
   return []
 }
 
