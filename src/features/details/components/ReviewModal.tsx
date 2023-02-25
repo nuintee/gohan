2c60c5673c683d44acdca4854cda14e4a32c64e6
nuintee <nuinteedev@gmail.com>
2023-02-25 18:56:42 +0300

    update: Input component text size responsive style/responsive#193

--- src/features/details/components/ReviewModal.tsx
+++ src/features/details/components/ReviewModal.tsx
@@ -74,13 +74,10 @@ const ReviewModal = ({ isOpen, onClose, data, onReviewSuccess }: Props) => {
 
   return (
     <ModalLayout isOpen={isOpen} onRequestClose={onClose}>
-      <section className='bg-white'>
+      <section className='bg-white w-[80vw] max-w-[30rem]'>
         <PanelHeader onClose={onClose} />
-        <form
-          onSubmit={onSubmit}
-          className='min-w-[30rem] p-4 flex flex-col items-center gap-8 justify-center'
-        >
-          <h2 className='font-semibold text-xl'>この場所への評価を教えて下さい。</h2>
+        <form onSubmit={onSubmit} className='p-4 flex flex-col items-center gap-8 justify-center'>
+          <h2 className='font-semibold sm:text-xl text-md'>この場所への評価を教えて下さい。</h2>
           <div className='flex items-center justify-center gap-4 mb-10'>
             <StatusRadioGroup status={data.status} register={register} />
           </div>
@@ -96,6 +93,29 @@ const ReviewModal = ({ isOpen, onClose, data, onReviewSuccess }: Props) => {
         </form>
       </section>
     </ModalLayout>
+    // <ModalLayout isOpen={isOpen} onRequestClose={onClose}>
+    //   <section className='bg-white'>
+    //     <PanelHeader onClose={onClose} />
+    //     <form
+    //       onSubmit={onSubmit}
+    //       className='min-w-[30rem] p-4 flex flex-col items-center gap-8 justify-center'
+    //     >
+    //       <h2 className='font-semibold text-xl'>この場所への評価を教えて下さい。</h2>
+    //       <div className='flex items-center justify-center gap-4 mb-10'>
+    //         <StatusRadioGroup status={data.status} register={register} />
+    //       </div>
+    //       <Input
+    //         type={'text'}
+    //         placeholder='この場所についてのメモを追加 (任意)'
+    //         defaultValue={data?.memo}
+    //         register={register}
+    //         registerName={'reviewMemo'}
+    //         required={false}
+    //       />
+    //       <Button text={'保存'} disabled={!isDirty} />
+    //     </form>
+    //   </section>
+    // </ModalLayout>
   )
 }
 
