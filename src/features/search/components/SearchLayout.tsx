d65fcb4488d8ebfb583b98b7d8c5c2e1552cd264
nuintee <nuinteedev@gmail.com>
2023-02-25 23:35:46 +0300

    update: search layout text style style/responsive#193

--- src/features/search/components/SearchLayout.tsx
+++ src/features/search/components/SearchLayout.tsx
@@ -55,14 +55,14 @@ const SearchLayout = ({ trigger = false }: { trigger?: boolean }) => {
       {/* Layout */}
       <div className='flex flex-col gap-2 items-center justify-center'>
         <h1
-          className={`text-2xl font-semibold sm:text-4xl ${
+          className={`text-xl font-semibold sm:text-4xl ${
             restaurants.isFetching && 'animate-fadeIn'
           }`}
         >
           {textUI()}
         </h1>
         {!restaurants.isFetching && !trigger && (
-          <p className='text-gh-d-gray text-lg'>クリックしてGohanする</p>
+          <p className='text-gh-d-gray sm:text-lg text-md'>クリックしてGohanする</p>
         )}
       </div>
       <GohanButton
