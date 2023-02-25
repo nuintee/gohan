ffe6c9e051c3541728f3fa81026ddbe32f577173
nuintee <nuinteedev@gmail.com>
2023-02-26 02:17:33 +0300

    add: animation to HeaderSidebar style/responsive#193

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -11,23 +11,26 @@ const HeaderSidebar = ({ isOpen = false, onClose }: { isOpen?: boolean; onClose?
   const router = useRouter()
 
   const slideIn = isOpen ? '-transform-x-full' : 'translate-x-full'
+  const opacity = isOpen ? 'bg-opacity-80' : 'bg-opacity-0 pointer-events-none'
 
   return (
-    <div
-      className={`bg-white absolute top-0 right-0 h-screen w-screen max-w-[30rem] duration-700 ${slideIn}`}
-      style={{
-        zIndex: '10000',
-      }}
-    >
-      <PanelHeader title='Gohan' onClose={onClose} />
-      <Link
-        href='/library'
-        className={`ml-auto p-2 items-center flex border-b-2 border-transparent sm:text-base text-sm hover:text-white active:text-white active:border-gh-orange hover:border-gh-orange text-gh-white ${
-          router.pathname === '/library' && 'border-gh-orange text-white'
-        }`}
+    <div className={`absolute h-screen w-screen bg-gh-dark z-[1010] duration-700 ${opacity}`}>
+      <div
+        className={`bg-white absolute top-0 right-0 h-screen w-screen max-w-[30rem] duration-700 ${slideIn}`}
+        style={{
+          zIndex: '10000',
+        }}
       >
-        ライブラリ
-      </Link>
+        <PanelHeader title='Gohan' onClose={onClose} />
+        <Link
+          href='/library'
+          className={`ml-auto p-2 items-center flex border-b-2 border-transparent sm:text-base text-sm hover:text-white active:text-white active:border-gh-orange hover:border-gh-orange text-gh-white ${
+            router.pathname === '/library' && 'border-gh-orange text-white'
+          }`}
+        >
+          ライブラリ
+        </Link>
+      </div>
     </div>
   )
 }
