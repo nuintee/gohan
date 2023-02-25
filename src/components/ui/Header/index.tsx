bfb89510b5cf72617819f7b8e1c281588d9d0573
nuintee <nuinteedev@gmail.com>
2023-02-26 01:11:12 +0300

    add: mediaQuery for Header Hamburger style/responsive#193

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -1,25 +1,40 @@
+import Hamburger from '@/components/icons/Hamburger'
 import User from '@/features/user/components/User'
+import useMediaQuery from '@/hooks/mediaquery'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import Brand from '../Brand'
 
 const Header = () => {
   const router = useRouter()
+  const isSmall = useMediaQuery('sm')
+
+  const mediaQuery = () => {
+    if (isSmall) {
+      return <Hamburger />
+    } else {
+      return (
+        <>
+          <Link
+            href='/library'
+            className={`ml-auto p-2 items-center flex border-b-2 border-transparent sm:text-base text-sm hover:text-white active:text-white active:border-gh-orange hover:border-gh-orange text-gh-white ${
+              router.pathname === '/library' && 'border-gh-orange text-white'
+            }`}
+          >
+            ライブラリ
+          </Link>
+          <div className='flex items-center'>
+            <User />
+          </div>
+        </>
+      )
+    }
+  }
 
   return (
-    <div className='flex gap-4 bg-gh-brown justify-between px-[10%] w-full'>
+    <div className='flex gap-4 bg-gh-brown justify-between px-[10%] w-full items-center sm:items-stretch'>
       <Brand margin={true} />
-      <Link
-        href='/library'
-        className={`ml-auto p-2 items-center flex border-b-2 border-transparent sm:text-base text-sm hover:text-white active:text-white active:border-gh-orange hover:border-gh-orange text-gh-white ${
-          router.pathname === '/library' && 'border-gh-orange text-white'
-        }`}
-      >
-        ライブラリ
-      </Link>
-      <div className='flex items-center'>
-        <User />
-      </div>
+      {mediaQuery()}
     </div>
   )
 }
