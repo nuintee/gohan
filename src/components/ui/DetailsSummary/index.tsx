013f74ba070d7938046a2b55b2c2ec742cf7d323
nuintee <nuinteedev@gmail.com>
2023-02-27 01:11:04 +0300

    add: CopyButton in DetailsSummary add/utils-#207

--- src/components/ui/DetailsSummary/index.tsx
+++ src/components/ui/DetailsSummary/index.tsx
@@ -1,22 +1,26 @@
 import { Chevron } from '@/components/icons'
+import CopyButton from '../CopyButton'
 
 const DetailsSummary = ({
   ignored = false,
   summaryTitle,
   summaryValue,
   children,
+  copyValue,
+  copyColor,
 }: {
   ignored?: boolean
   summaryTitle?: string
   summaryValue?: string
   children: JSX.Element
-}) => {
+} & React.ComponentProps<typeof CopyButton>) => {
   return (
     <details className={`w-full group ${ignored && 'pointer-events-none'}`}>
-      <summary className='flex items-center justify-between  cursor-pointer'>
+      <summary className='flex items-center justify-between gap-4  cursor-pointer'>
         <div className='flex items-center gap-2'>
           {!ignored && <Chevron overrideClassName='rotate-90 group-open:-rotate-90 ml-1' />}
           <p>{summaryTitle}</p>
+          {copyValue && <CopyButton copyColor={copyColor} copyValue={summaryValue} />}
         </div>
         <h2>{summaryValue}</h2>
       </summary>
