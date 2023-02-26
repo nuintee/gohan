12c2963bf8641b9d49efaf8e62dd7640734aca68
nuintee <nuinteedev@gmail.com>
2023-02-27 00:46:12 +0300

    update: copy function with copyText add/utils-#207

--- src/utils/copy.ts
+++ src/utils/copy.ts
@@ -1,8 +1,8 @@
 import useToast from '@/libs/react-toastify'
 
-export async function copy(data: ClipboardItems) {
+export async function copy(text: string) {
   try {
-    await navigator.clipboard.write(data)
+    await navigator.clipboard.writeText(text)
     useToast.success('コピー完了')
   } catch (error) {
     useToast.error('コピーに失敗しました。')
