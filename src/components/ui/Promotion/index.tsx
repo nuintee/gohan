3b235af9a4d30bbe3d0c98de13964e6c1802014d
nuintee <nuinteedev@gmail.com>
2023-02-27 06:03:36 +0300

    update: Promotion component gradient prod/adjustment#206

--- src/components/ui/Promotion/index.tsx
+++ src/components/ui/Promotion/index.tsx
@@ -1,7 +1,7 @@
 const Promotion = () => {
   return (
-    <div className='p-4  bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100 rounded-md h-[7.5rem] shadow-md duration-700 hover:scale-105 cursor-pointer'>
-      <p className='font-semibold select-none'>
+    <div className='p-4 flex items-center justify-center bg-gradient-to-r from-pink-500 via-gh-red to-gh-orange rounded-md h-[7.5rem] shadow-md duration-700 hover:scale-105 cursor-pointer'>
+      <p className='font-semibold select-none text-white'>
         アカウントを作成してお気に入りの場所を保存しよう。
       </p>
     </div>
