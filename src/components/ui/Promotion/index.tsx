843a1f1b268fad1b94497fe32ed5db91bda16435
nuintee <nuinteedev@gmail.com>
2023-02-27 06:06:52 +0300

    update: Promotion text prod/adjustment#206

--- src/components/ui/Promotion/index.tsx
+++ src/components/ui/Promotion/index.tsx
@@ -6,7 +6,7 @@ const Promotion = () => {
       onClick={() => signIn('google')}
       className='p-4 flex items-center w-full justify-center bg-gradient-to-r from-pink-500 via-gh-red to-gh-orange rounded-md h-[7.5rem] shadow-md duration-700 hover:scale-105 cursor-pointer font-semibold select-none text-white'
     >
-      アカウントを作成してお気に入りの場所を保存しよう。
+      簡単にアカウントを作成してお気に入りの場所を保存しよう。
     </button>
   )
 }
