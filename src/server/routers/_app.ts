c853aac9ca21958ab4dcbabd5a2fa3bff7108e40
nuintee <nuinteedev@gmail.com>
2023-02-27 05:04:33 +0300

    remove: experiments api prod/adjustment#206

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -4,13 +4,11 @@ import { router } from '../trpc'
 import * as activities from '@/features/activities/api'
 import * as user from '@/features/user/api'
 import * as restaurants from '@/features/restaurants/api'
-import * as experiments from './experiment'
 
 export const appRouter = router({
   ...activities,
   ...user,
   ...restaurants,
-  ...experiments,
 })
 
 export type AppRouter = typeof appRouter
