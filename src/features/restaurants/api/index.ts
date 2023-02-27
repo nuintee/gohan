bb7753f6330b1e3508dc7c3dc60fea4597432075
nuintee <nuinteedev@gmail.com>
2023-02-27 18:04:08 +0300

    add: throwing error when responce is not ok prod/adjustment#206

--- src/features/restaurants/api/index.ts
+++ src/features/restaurants/api/index.ts
@@ -17,6 +17,8 @@ import { NEEDED_DETAIL_FIELDS } from '../constants'
 import { sleep } from '@/utils/sleep'
 import { trpc } from '@/libs/trpc'
 import { DetailsAPI, PlacesAPI } from '../types'
+import { getBarePlacesAPI } from '../utils/getBarePlacesAPI'
+import { TRPCClientError } from '@trpc/client'
 
 const validInput = z.object({
   place_id: z.optional(z.string()),
@@ -40,51 +42,61 @@ const anyValid = validInput.refine(
   { message: 'Must provide any' },
 )
 
+// export const getRestaurants = procedure.input(anyValid).query(async ({ input }) => {
+//   const { isPlaceId } = checkIsAnyValid(input)
+//   if (IS_DEVMODE) {
+//     await sleep(1000)
+
+//     if (isPlaceId) {
+//       return details.result(input?.place_id as string)
+//     } else {
+//       const openNow = restaurantsData.results.filter((v) => v.opening_hours.open_now)
+//       const randomOne = openNow[Math.floor(Math.random() * openNow.length)]
+
+//       return randomOne
+//     }
+//   } else if (IS_PRODMODE) {
+//     if (isPlaceId) {
+//       // return details
+
+//       const url = new URL('https://maps.googleapis.com/maps/api/place/details/json')
+//       url.searchParams.append('place_id', input?.place_id as string)
+//       url.searchParams.append('key', GCP_API_KEY)
+//       url.searchParams.append('fields', NEEDED_DETAIL_FIELDS.join(',')) // Needed Fields
+
+//       const { data } = await axios.get<DetailsAPI>(url.toString())
+
+//       if (!['OK', 'ZERO_RESULTS', 'INVALID_REQUEST', 'OVER_QUERY_LIMIT'].includes(data.status))
+//         throw new Error(data.status)
+
+//       return data.result
+//     } else {
+//       // return random
+//       const url = new URL('https://maps.googleapis.com/maps/api/place/nearbysearch/json')
+//       url.searchParams.append('location', `${input.latitude},${input.longitude}`)
+//       url.searchParams.append('radius', '500')
+//       url.searchParams.append('types', 'restaurant') // old types was food
+//       url.searchParams.append('opennow', 'true')
+//       url.searchParams.append('key', GCP_API_KEY)
+
+//       const { data } = await axios.get<PlacesAPI>(url.toString())
+
+//       if (!['OK', 'ZERO_RESULTS'].includes(data.status)) throw new Error(data.status)
+
+//       const open_now = data.results?.filter((v) => v.opening_hours?.open_now) || []
+//       const random = open_now[Math.floor(Math.random() * open_now.length - 1)]
+//       return random
+//     }
+//   } else {
+//   }
+// })
 export const getRestaurants = procedure.input(anyValid).query(async ({ input }) => {
   const { isPlaceId } = checkIsAnyValid(input)
-  if (IS_DEVMODE) {
-    await sleep(1000)
+  const data = await getBarePlacesAPI({ latitude: input.latitude, longitude: input.longitude })
 
-    if (isPlaceId) {
-      return details.result(input?.place_id as string)
-    } else {
-      const openNow = restaurantsData.results.filter((v) => v.opening_hours.open_now)
-      const randomOne = openNow[Math.floor(Math.random() * openNow.length)]
-
-      return randomOne
-    }
-  } else if (IS_PRODMODE) {
-    if (isPlaceId) {
-      // return details
-
-      const url = new URL('https://maps.googleapis.com/maps/api/place/details/json')
-      url.searchParams.append('place_id', input?.place_id as string)
-      url.searchParams.append('key', GCP_API_KEY)
-      url.searchParams.append('fields', NEEDED_DETAIL_FIELDS.join(',')) // Needed Fields
-
-      const { data } = await axios.get<DetailsAPI>(url.toString())
-
-      if (!['OK', 'ZERO_RESULTS', 'INVALID_REQUEST', 'OVER_QUERY_LIMIT'].includes(data.status))
-        throw new Error(data.status)
-
-      return data.result
-    } else {
-      // return random
-      const url = new URL('https://maps.googleapis.com/maps/api/place/nearbysearch/json')
-      url.searchParams.append('location', `${input.latitude},${input.longitude}`)
-      url.searchParams.append('radius', '500')
-      url.searchParams.append('types', 'restaurant') // old types was food
-      url.searchParams.append('opennow', 'true')
-      url.searchParams.append('key', GCP_API_KEY)
-
-      const { data } = await axios.get<PlacesAPI>(url.toString())
-
-      if (!['OK', 'ZERO_RESULTS'].includes(data.status)) throw new Error(data.status)
-
-      const open_now = data.results?.filter((v) => v.opening_hours?.open_now) || []
-      const random = open_now[Math.floor(Math.random() * open_now.length - 1)]
-      return random
-    }
+  if (data.status === 'OK') {
+    return data.results?.random() // returnRandomOne
   } else {
+    throw new TRPCClientError(data.status)
   }
 })
