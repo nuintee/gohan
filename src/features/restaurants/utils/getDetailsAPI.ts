32ecabcbe133d98f3086624f3de55d9056947116
nuintee <nuinteedev@gmail.com>
2023-02-27 17:40:46 +0300

    add: prod and dev mode switcher in getDetails prod/adjustment#206

--- src/features/restaurants/utils/getDetailsAPI.ts
+++ src/features/restaurants/utils/getDetailsAPI.ts
@@ -1,17 +1,21 @@
-import { GCP_API_KEY } from '@/config/env'
+import { GCP_API_KEY, IS_PRODMODE } from '@/config/env'
 import axios from '@/libs/axios'
 import { NEEDED_DETAIL_FIELDS } from '../constants'
 import { DetailsAPI, ResultsEntity } from '../types'
 
 async function getDetailsAPI<T extends ResultsEntity['place_id']>({ place_id }: { place_id: T }) {
-  const url = new URL('https://maps.googleapis.com/maps/api/place/details/json')
-  url.searchParams.append('place_id', place_id)
-  url.searchParams.append('key', GCP_API_KEY)
-  url.searchParams.append('fields', NEEDED_DETAIL_FIELDS.join(',')) // Needed Fields
+  if (IS_PRODMODE) {
+    const url = new URL('https://maps.googleapis.com/maps/api/place/details/json')
+    url.searchParams.append('place_id', place_id)
+    url.searchParams.append('key', GCP_API_KEY)
+    url.searchParams.append('fields', NEEDED_DETAIL_FIELDS.join(',')) // Needed Fields
 
-  const { data } = await axios.get<DetailsAPI>(url.toString())
+    const { data } = await axios.get<DetailsAPI>(url.toString())
 
-  return data
+    return data
+  } else {
+    return {}
+  }
 }
 
 export default getDetailsAPI
--- src/features/restaurants/utils/getPlacesAPI.ts
+++ src/features/restaurants/utils/getPlacesAPI.ts
@@ -1,4 +1,4 @@
-import { GCP_API_KEY, IS_DEVMODE } from '@/config/env'
+import { GCP_API_KEY, IS_DEVMODE, IS_PRODMODE } from '@/config/env'
 import { IS_BROWSER } from '@/config/mode'
 import axios from '@/libs/axios'
 import { sleep } from '@/utils/sleep'
@@ -7,16 +7,20 @@ import { PlacesAPI } from '../types'
 async function getPlacesAPI<T extends Pick<GeolocationCoordinates, 'latitude' | 'longitude'>>(
   props: T,
 ) {
-  const url = new URL('https://maps.googleapis.com/maps/api/place/nearbysearch/json')
-  url.searchParams.append('location', `${props.latitude},${props.longitude}`)
-  url.searchParams.append('radius', '500')
-  url.searchParams.append('types', 'restaurant') // old types was food
-  url.searchParams.append('opennow', 'true')
-  url.searchParams.append('key', GCP_API_KEY)
+  if (IS_PRODMODE) {
+    const url = new URL('https://maps.googleapis.com/maps/api/place/nearbysearch/json')
+    url.searchParams.append('location', `${props.latitude},${props.longitude}`)
+    url.searchParams.append('radius', '500')
+    url.searchParams.append('types', 'restaurant') // old types was food
+    url.searchParams.append('opennow', 'true')
+    url.searchParams.append('key', GCP_API_KEY)
 
-  const { data } = await axios.get<PlacesAPI>(url.toString())
+    const { data } = await axios.get<PlacesAPI>(url.toString())
 
-  return data
+    return data
+  } else {
+    return {}
+  }
 }
 
 export default getPlacesAPI
