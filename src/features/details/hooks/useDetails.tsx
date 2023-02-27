8da0a0612e20e51667e24813b03c13c179560ac8
nuintee <nuinteedev@gmail.com>
2023-02-27 18:25:23 +0300

    add: useDetails hook trpc/restaurants#225

--- src/features/details/hooks/useDetails.tsx
+++ src/features/details/hooks/useDetails.tsx
@@ -1,3 +1,27 @@
-const useDetails = () => {}
+import useToast from '@/libs/react-toastify'
+import { trpc } from '@/libs/trpc'
+
+const useDetails = ({
+  trigger,
+  successCallback,
+  ...rest
+}: Parameters<typeof trpc.getDetails.useQuery>[0] & {
+  successCallback?: (data: ReturnType<typeof trpc.getDetails.useQuery>) => void
+  trigger?: boolean
+}) => {
+  return trpc.getDetails.useQuery(rest, {
+    enabled: trigger,
+    onError: (error) => {
+      console.error(error)
+
+      if (error instanceof Error) {
+        useToast.error(error)
+      }
+    },
+    onSuccess: (data) => {
+      successCallback && successCallback(data)
+    },
+  })
+}
 
 export default useDetails
