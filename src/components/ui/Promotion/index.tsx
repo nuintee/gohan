3c4717abfb9b71083d55da580a3c31ebd8155d1b
nuintee <nuinteedev@gmail.com>
2023-02-27 06:05:29 +0300

    add: signin to promotion onClick prod/adjustment#206

--- src/components/ui/Promotion/index.tsx
+++ src/components/ui/Promotion/index.tsx
@@ -1,10 +1,13 @@
+import { signIn } from 'next-auth/react'
+
 const Promotion = () => {
   return (
-    <div className='p-4 flex items-center justify-center bg-gradient-to-r from-pink-500 via-gh-red to-gh-orange rounded-md h-[7.5rem] shadow-md duration-700 hover:scale-105 cursor-pointer'>
-      <p className='font-semibold select-none text-white'>
-        アカウントを作成してお気に入りの場所を保存しよう。
-      </p>
-    </div>
+    <button
+      onClick={() => signIn('google')}
+      className='p-4 flex items-center w-full justify-center bg-gradient-to-r from-pink-500 via-gh-red to-gh-orange rounded-md h-[7.5rem] shadow-md duration-700 hover:scale-105 cursor-pointer font-semibold select-none text-white'
+    >
+      アカウントを作成してお気に入りの場所を保存しよう。
+    </button>
   )
 }
 
