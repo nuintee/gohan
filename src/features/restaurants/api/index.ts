3e9c993e6d478a14d02cfc737e9e252e0ccd3f40
nuintee <nuinteedev@gmail.com>
2023-02-27 22:56:21 +0300

    add: statusMapper in getRestaurants API prod/adjustment#206

--- src/features/restaurants/api/index.ts
+++ src/features/restaurants/api/index.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 
 import { getBarePlacesAPI } from '../utils/getBarePlacesAPI'
 import { TRPCClientError } from '@trpc/client'
+import { statusMapper } from '../utils/statusMapper'
 
 export const getRestaurants = procedure
   .input(
@@ -17,6 +18,6 @@ export const getRestaurants = procedure
     if (data.status === 'OK') {
       return data.results?.random() // returnRandomOne
     } else {
-      throw new TRPCClientError(data.status)
+      throw new TRPCClientError(statusMapper(data.status), { cause: new Error(data.status) })
     }
   })
