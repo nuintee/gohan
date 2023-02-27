2d94e1d713f8b50f0be96c977ef73b8beb39e5d9
nuintee <nuinteedev@gmail.com>
2023-02-27 03:29:29 +0300

    update: getActivity in different file prod/adjustment#206

--- src/features/activities/api/index.ts
+++ src/features/activities/api/index.ts
@@ -11,97 +11,8 @@ import { sleep } from '@/utils/sleep'
 
 // data
 import { details as detailsData } from '@/data/details'
-import { TRPCError } from '@trpc/server'
-import {
-  DetailsAPI,
-  PlacesDetailsStatus,
-  PLACES_RESPONSE_STATUS,
-} from '@/features/restaurants/types'
 import { isAuthedMiddleWare } from '@/server/middleware'
-
-export const getActivity = procedure
-  .input(
-    z.object({
-      userId: z.optional(z.string()),
-      place_id: z.string(),
-    }),
-  )
-  .query(async ({ input }) => {
-    if (IS_DEVMODE) {
-      let data
-
-      if (input.userId) {
-        // Get activity when authed
-
-        data = await prisma.activity.findUnique({
-          where: {
-            userId_place_id: {
-              userId: input.userId,
-              place_id: input.place_id,
-            },
-          },
-        })
-      }
-
-      const detailsResult = detailsData.result(input?.place_id)
-
-      const detailsResponse = (): PlacesDetailsStatus => {
-        if (!detailsResult) {
-          return 'ZERO_RESULTS'
-        } else {
-          return 'OK'
-        }
-      }
-
-      const detailsErrorMessage = (status: PlacesDetailsStatus) => {
-        switch (status) {
-          case 'ZERO_RESULTS':
-            return '該当するデータが見つかりませんでした。'
-          case 'OVER_QUERY_LIMIT':
-            return 'API通信回数制限を超えました。'
-          case 'REQUEST_DENIED':
-            return 'リクエストが拒否されました。'
-          case 'UNKNOWN_ERROR':
-            return '予期しないエラーが発生しました。'
-          case 'INVALID_REQUEST':
-            return '無効なリクエストです。'
-          default:
-            return ''
-        }
-      }
-
-      const detailedResponse: DetailsAPI = {
-        ...detailsData,
-        result: detailsResult || {},
-        status: detailsResponse(),
-      }
-
-      if (detailedResponse.status !== 'OK') {
-        // throw TRPC Error
-        throw new TRPCError({
-          code: 'BAD_REQUEST',
-          cause: detailedResponse.status,
-          message: detailsErrorMessage(detailedResponse.status),
-        })
-      }
-
-      const result = { ...data, ...detailedResponse.result }
-
-      if (!result || !Object.keys(result).length) {
-        throw new TRPCError({
-          code: 'BAD_REQUEST',
-          cause: detailedResponse.status,
-          message: detailsErrorMessage('ZERO_RESULTS'),
-        })
-      }
-
-      return result
-    } else if (IS_PRODMODE) {
-      // Google API
-    } else {
-      // test
-    }
-  })
+import { getActivity } from './getActivity'
 
 export const deleteActivity = procedure
   .use(isAuthedMiddleWare)
@@ -207,3 +118,5 @@ export const getUserActivities = procedure
       // test
     }
   })
+
+export { getActivity }
