25be118f9681317a1c82bc628c9077cf1bb083ad
nuintee <nuinteedev@gmail.com>
2023-03-01 06:09:06 +0300

    add: Details mobile Loading Fallback style/mobile#236

--- src/features/details/components/DetailsLoadingFallback.tsx
+++ src/features/details/components/DetailsLoadingFallback.tsx
@@ -15,44 +15,15 @@ import DetailsActionGroup from './ui/DetailsActionGroup'
 const DetailsLoadingFallback = () => {
   const isOverSmall = useMediaQuery('sm')
 
-  const smallClassName = {
-    container: '',
-    subContainer: 'px-[10%] pt-8 pb-5 flex gap-4 flex-col items-center justify-center',
-    actionGroupContainer: 'flex items-center justify-center my-2',
-  }
-
-  const mediumClassName = {
-    container: 'px-[10%] pt-16 pb-6 flex gap-8',
-    subContainer: '',
-    actionGroupContainer: 'flex-1 flex flex-col justify-between py-2 min-h-[14rem] overflow-x-clip',
-  }
-
-  const theme = !isOverSmall ? smallClassName : mediumClassName
+  const heroClassName = !isOverSmall
+    ? 'pt-8 pb-5 flex gap-4 flex-col items-center justify-center'
+    : 'px-[10%] pt-16 pb-24 flex gap-8'
 
   return (
     <>
-      {/* <div className='flex flex-1 flex-col relative'>
-        <Cover />
-        <div className='px-[10%] pt-16 pb-24 flex gap-8'>
-          <ImageChip isLoading={true} />
-          <div className='flex-1 flex flex-col justify-between py-2'>
-            <Texts isLoading={true} main='LOADING_TEXTS' />
-          </div>
-        </div>
-        <main className='px-[10%]'>
-          <Texts main='LOADING_TEXTS' isLoading={true} />
-          <section className='flex items-center justify-between gap-4 my-14'>
-            <DescriptiveChip isLoading={true} />
-            <DescriptiveChip isLoading={true} />
-            <DescriptiveChip isLoading={true} />
-          </section>
-          <DetailsSection isLoading={true} main='LOADING_SECTION' />
-          <DetailsSection isLoading={true} main='LOADING_SECTION' />
-        </main>
-      </div> */}
       <div className='flex flex-1 flex-col relative'>
         <Cover />
-        <div className={theme.subContainer}>
+        <div className={heroClassName}>
           <ImageChip isLoading={true} />
           <div className='flex-1 flex flex-col justify-between py-2'>
             <Texts isLoading={true} main='LOADING_TEXTS' size='small' />
