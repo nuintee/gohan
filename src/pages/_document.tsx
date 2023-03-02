da26129b28575dc3a540aa205d48a9d461190285
nuintee <nuinteedev@gmail.com>
2023-03-03 00:30:39 +0300

    add: NoIndex when staging is true dev

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import { IS_PRODMODE } from '@/config/env'
+import { IS_PRODMODE, IS_STAGING } from '@/config/env'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
@@ -15,7 +15,6 @@ class MyDocument extends Document {
         <Head>
           <link rel='manifest' href='/manifest.json' />
           <link rel='apple-touch-icon' href='/icon.png'></link>
-          <meta name='theme-color' content='#fff' />
           <link rel='icon' href='prod_favicon.svg' />
           <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
@@ -23,9 +22,11 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap'
             rel='stylesheet'
           />
+          <meta name='theme-color' content='#fff' />
           <meta name='twitter:card' content='summary' />
           <meta name='twitter:site' content='@tcr_jp' />
           <meta property='og:type' content='blog' />
+          {IS_STAGING && <meta name='robots' content='noindex' />}
         </Head>
         <body>
           <Main />
