fcaeaec1b65f6b3eb4bf696c3473567681845556
nuintee <nuinteedev@gmail.com>
2023-03-02 23:44:05 +0300

    add: Basic auth conditional rendering depends on STAGING state dev

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,13 @@
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
+import { STAGING } from './config/env'
 
 const BASIC_AUTH_USER = 'USER'
 const BASIC_AUTH_PASSWORD = 'PSSWRD'
 
 export const middleware = (req: NextRequest) => {
+  if (STAGING !== 'true') return NextResponse.next()
+
   const authorizationHeader = req.headers.get('authorization')
 
   if (authorizationHeader) {
