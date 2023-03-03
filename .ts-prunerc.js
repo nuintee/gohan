003147fbac08754ee9193ca5f91832fb46ca1a23
nuintee <nuinteedev@gmail.com>
2023-03-04 00:39:13 +0300

    update: matches in prune rc optimize/minify#246

--- .ts-prunerc.js
+++ .ts-prunerc.js
@@ -1,13 +1,8 @@
-const mathes = [
-  'default',
-  'Default',
-  'getServersideProps',
-  'middleware',
-  'wrapper',
-  'serUpWrapper',
-  'decorators',
-  'stories.(tsx|jsx|ts|js)',
-]
+const _SERVERSIDE = ['getServersideProps', 'middleware']
+const _STORYBOOK = ['wrapper', 'setUpWrapepr', 'Default', 'decorators', 'stories.(tsx|jsx|ts|js)']
+const _DEFAULT = ['default']
+
+const mathes = [..._SERVERSIDE, ..._STORYBOOK, ..._DEFAULT]
 
 module.exports = {
   ignore: mathes.join('|'),
