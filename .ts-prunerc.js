e77ca5229be43cc0303621aa9bf5bcd0e9c7e498
nuintee <nuinteedev@gmail.com>
2023-03-04 00:36:53 +0300

    add: dynamic ignore path in ts prune rc optimize/minify#246

--- .ts-prunerc.js
+++ .ts-prunerc.js
@@ -1,4 +1,14 @@
+const mathes = [
+  'default',
+  'Default',
+  'getServersideProps',
+  'middleware',
+  'wrapper',
+  'serUpWrapper',
+  'decorators',
+  'stories.(tsx|jsx|ts|js)',
+]
+
 module.exports = {
-  ignore:
-    '(default|Default|getServerSideProps|middleware|getServerSideProps|wrapper|setUpWrapper|decorators|stories.(tsx|jsx|ts|js))',
+  ignore: mathes.join('|'),
 }
