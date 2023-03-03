f8aa88991510edbb47c3f77c6fd480643a5048bb
nuintee <nuinteedev@gmail.com>
2023-03-03 21:55:36 +0300

    add: offline tsx and runtime cahching for PWA dev

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
+const runtimeCaching = require('next-pwa/cache')
 const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
+  runtimeCaching,
 })
 
 /** @type {import('next').NextConfig} */
