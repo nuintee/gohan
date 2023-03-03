bd97facddb1e2b2a0b545cef398a67d71ec59d47
nuintee <nuinteedev@gmail.com>
2023-03-03 03:04:42 +0300

    add: trpc base url staging

--- src/libs/trpc.ts
+++ src/libs/trpc.ts
@@ -1,6 +1,7 @@
 import { httpBatchLink } from '@trpc/client'
 import { createTRPCNext } from '@trpc/next'
 import type { AppRouter } from '@/server/routers/_app'
+import { BASE_URL } from '@/config/env'
 
 function getBaseUrl() {
   if (typeof window !== 'undefined')
@@ -24,7 +25,7 @@ export const trpc = createTRPCNext<AppRouter>({
     return {
       links: [
         httpBatchLink({
-          url: `${getBaseUrl()}/api/trpc`,
+          url: `${BASE_URL}/api/trpc`,
         }),
       ],
       abortOnUnmount: true,
