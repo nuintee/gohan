92defaf1be57280fa94bdfc8a1f3f6bcfa54c17c
nuintee <nuinteedev@gmail.com>
2023-03-03 03:16:21 +0300

    removed: getDominantColor function staging

--- src/libs/rgbaster.ts
+++ src/libs/rgbaster.ts
@@ -4,23 +4,23 @@ import analyze from 'rgbaster'
 async function getDominantColor(url: string) {
   const FALLBACK_COLOR = colors['gh-dark']
 
-  if (!url) return FALLBACK_COLOR
+  // if (!url) return FALLBACK_COLOR
 
-  const task = new Promise<string>((resolve, reject) => {
-    analyze(url, { scale: 0.2 }).then((data) => {
-      resolve(data[0].color)
-    })
-  })
+  // const task = new Promise<string>((resolve, reject) => {
+  //   analyze(url, { scale: 0.2 }).then((data) => {
+  //     resolve(data[0].color)
+  //   })
+  // })
 
-  const _racer = new Promise<string>((resolve, reject) => {
-    setTimeout(() => {
-      resolve(FALLBACK_COLOR)
-    }, 2000)
-  })
+  // const _racer = new Promise<string>((resolve, reject) => {
+  //   setTimeout(() => {
+  //     resolve(FALLBACK_COLOR)
+  //   }, 2000)
+  // })
 
-  const value = await Promise.race([task, _racer])
+  // const value = await Promise.race([task, _racer])
 
-  return value
+  return FALLBACK_COLOR
 }
 
 export { getDominantColor }
