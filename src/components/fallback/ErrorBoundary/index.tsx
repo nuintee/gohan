2fb295dc32207bb15ae86efbe86435eaacf9d4e5
nuintee <nuinteedev@gmail.com>
2023-03-03 08:58:03 +0300

    update: replaced error boundary fallback to custom error fallback dev

--- src/components/fallback/ErrorBoundary/index.tsx
+++ src/components/fallback/ErrorBoundary/index.tsx
@@ -1,19 +1,10 @@
 import React, { Children } from 'react'
 
 import { ErrorBoundary as Boundary } from 'react-error-boundary'
-
-function ErrorFallback({ error, resetErrorBoundary }) {
-  return (
-    <div role='alert'>
-      <p>Something went wrong:</p>
-      <pre>{error.message}</pre>
-      <button onClick={resetErrorBoundary}>Try again</button>
-    </div>
-  )
-}
+import ErrorFallBack from '../ErrorFallback'
 
 const ErrorBoundary = ({ children }: { children: React.ReactChild }) => {
-  return <Boundary FallbackComponent={ErrorFallback}>{children}</Boundary>
+  return <Boundary FallbackComponent={ErrorFallBack}>{children}</Boundary>
 }
 
 export default ErrorBoundary
--- src/components/fallback/ErrorFallback/index.tsx
+++ src/components/fallback/ErrorFallback/index.tsx
@@ -6,13 +6,24 @@ import { useRouter } from 'next/router'
 // constants
 import { ROUTES } from '@/constants/routes'
 
-const ErrorFallBack = ({ error }: { error: TRPCClientErrorBase<DefaultErrorShape> | Error }) => {
+const ErrorFallBack = ({
+  error,
+  resetErrorBoundary,
+}: {
+  error: TRPCClientErrorBase<DefaultErrorShape> | Error
+  resetErrorBoundary?: (args: unknown) => void
+}) => {
   const router = useRouter()
 
   return (
     <div className='h-screen w-screen flex flex-col gap-4 items-center justify-center'>
       <h1>エラー</h1>
       <p>{error.message}</p>
+      {resetErrorBoundary && (
+        <button className='underline' onClick={resetErrorBoundary}>
+          リトライ
+        </button>
+      )}
       <div>
         <Button text='ホームへ戻る' onClick={() => router.push(ROUTES.HOME.path)} />
       </div>
