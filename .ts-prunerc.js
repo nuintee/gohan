ce32f15a140febdfc0c48e80d4760baf90989c52
nuintee <nuinteedev@gmail.com>
2023-03-04 00:39:40 +0300

    update: type in matches optimize/minify#246

--- .ts-prunerc.js
+++ .ts-prunerc.js
@@ -2,8 +2,8 @@ const _SERVERSIDE = ['getServersideProps', 'middleware']
 const _STORYBOOK = ['wrapper', 'setUpWrapepr', 'Default', 'decorators', 'stories.(tsx|jsx|ts|js)']
 const _DEFAULT = ['default']
 
-const mathes = [..._SERVERSIDE, ..._STORYBOOK, ..._DEFAULT]
+const matches = [..._SERVERSIDE, ..._STORYBOOK, ..._DEFAULT]
 
 module.exports = {
-  ignore: mathes.join('|'),
+  ignore: matches.join('|'),
 }
