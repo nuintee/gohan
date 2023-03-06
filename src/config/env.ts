c71c06412a42f611fa4af93234c59d5ec2b0237f
nuintee <nuinteedev@gmail.com>
2023-03-06 08:53:47 +0300

    update: GCP_API_KEY env#249

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,5 @@
 export const MAPBOX_PUBLIC_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_PUBLIC_TOKEN as string
-export const GCP_API_KEY = process.env.NEXT_PUBLIC_GCP_API_KEY as string
+export const GCP_API_KEY = process.env.GCP_API_KEY as string
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 export const AXIOS_FETCH_TIMEOUT = parseInt(process.env.NEXT_PUBLIC_AXIOS_FETCH_TIMEOUT as string)
 export const IS_DEVMODE = process.env.NODE_ENV === 'development'
