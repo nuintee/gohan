ab6493a84fd488e3fbaaf8b65a7fc605c426ba60
nuintee <nuinteedev@gmail.com>
2023-03-07 06:25:17 +0300

    add: tested in _index with client using env#249

--- src/pages/_index.tsx
+++ src/pages/_index.tsx
@@ -15,7 +15,14 @@ const Experiment = () => {
   return (
     <div className='flex flex-col'>
       {/* <Image width={400} src={'https://maps.googleapis.com/maps/api/place/photo'} alt='' /> */}
-      <img width={400} height={400} src={getImage.data} alt='' />
+      <img
+        width={400}
+        height={400}
+        src={
+          'http://localhost:3000/api/image/Aap_uEA7vb0DDYVJWEaX3O-AtYp77AaswQKSGtDaimt3gt7QCNpdjp1BkdM6acJ96xTec3tsV_ZJNL_JP-lqsVxydG3nh739RE_hepOOL05tfJh2_ranjMadb3VoBYFvF0ma6S24qZ6QJUuV6sSRrhCskSBP5C1myCzsebztMfGvm7ij3gZT'
+        }
+        alt=''
+      />
     </div>
   )
 }
