88b4f8458d97b8230153a2c32e93a99e8e730bba
nuintee <nuinteedev@gmail.com>
2023-03-08 18:54:21 +0300

    update: Tooltip style dev

--- src/components/ui/Tootltip/index.tsx
+++ src/components/ui/Tootltip/index.tsx
@@ -3,7 +3,7 @@ import DropDownLayout from '@/layouts/DropDownLayout'
 const ToolTip = ({ text }: { text?: string }) => {
   return (
     <DropDownLayout
-      overrideStyle='bg-gh-orange p-4 rounded-md shadow-sm mb-4'
+      overrideStyle='bg-gh-gray px-4 py-2 rounded-md shadow-sm mb-2'
       controller={
         <div className='h-6 w-6 bg-gh-dark rounded-full flex items-center justify-center text-white'>
           i
@@ -12,8 +12,8 @@ const ToolTip = ({ text }: { text?: string }) => {
       direction={'top'}
     >
       <>
-        <p className='text-sm text-white'>{text}</p>
-        <div className='absolute bottom-0 right-0 bg-gh-orange h-3 w-3 rotate-45 translate-y-1/2 -translate-x-full'></div>
+        <p className='text-sm text-gh-white'>{text}</p>
+        <div className='absolute bottom-0 right-0 bg-gh-gray h-3 w-3 rotate-45 translate-y-1/2 -translate-x-full'></div>
       </>
     </DropDownLayout>
   )
