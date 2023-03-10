9f6eb03449dca2bf1fbd56f3e772be80d8c8a08d
nuintee <nuinteedev@gmail.com>
2023-03-10 16:10:56 +0300

    fix: SORT_ENUM sortFn ts error types/fix-all#239

--- src/constants/sort.ts
+++ src/constants/sort.ts
@@ -1,22 +1,22 @@
 export const SORT_ENUM = {
   ASC: {
     label: '昇順',
-    sortFn: (a, b, sortKey) => {
+    sortFn: <T extends Object, K extends keyof T>(a: T, b: T, sortKey: K) => {
       if (a[sortKey] < b[sortKey]) {
         return -1
       }
-      if (a[sortKey] > b.name) {
+      if (a[sortKey] > b[sortKey]) {
         return 1
       }
     },
   },
   DESC: {
     label: '降順',
-    sortFn: (a, b, sortKey) => {
+    sortFn: <T extends Object, K extends keyof T>(a: T, b: T, sortKey: K) => {
       if (a[sortKey] < b[sortKey]) {
         return 1
       }
-      if (a[sortKey] > b.name) {
+      if (a[sortKey] > b[sortKey]) {
         return -1
       }
     },
