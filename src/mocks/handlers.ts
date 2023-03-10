f81f53a270e09ba501edc7d9806e7799bc86d0b5
nuintee <nuinteedev@gmail.com>
2023-03-10 18:16:48 +0300

    type: googlePlacesHandler types/fix-all#239

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -10,11 +10,4 @@ export const handlers = [
   rest.get(`https://maps.googleapis.com/maps/api/place/details/json`, googleDetailsAPIHandlers),
   rest.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json`, googlePlacesHandlers),
   rest.get(`https://jsonplaceholder.typicode.com/todos/1`, googlePlacesHandlers),
-  // rest.get(`${BASE_URL}/api/trpc/getActivity`, (req, res, ctx) => {
-  //   const input = req.url.searchParams.get('input')
-  //   const decoded = decodeURI(input)
-  //   const parsed = JSON.parse(decoded)
-  //   console.log(parsed)
-  //   return res(ctx.json({}))
-  // }),
 ]
