487055a87994d1334206e4e234ef7c8dff9c1f98
nuintee <nuinteedev@gmail.com>
2023-03-10 18:07:28 +0300

    remove: activities types/fix-all#239

--- src/features/activities/components/ActivityPanel.stories.tsx
+++ src/features/activities/components/ActivityPanel.stories.tsx
@@ -5,33 +5,14 @@ import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 
 import ActivityPanel from './ActivityPanel'
-import { activities } from '@/data/activities'
 
 export default {
   title: 'Features/Activities/Panel',
   component: ActivityPanel,
 } as ComponentMeta<typeof ActivityPanel>
 
-const Template: ComponentStory<typeof ActivityPanel> = (args) => <ActivityPanel {...args} />
+const Template: ComponentStory<typeof ActivityPanel> = () => <ActivityPanel />
 
 export const Default = Template.bind({})
-export const Loading = Template.bind({})
-export const Empty = Template.bind({})
 
-Default.args = {
-  query: {
-    data: [...activities, ...activities, ...activities],
-  },
-}
-
-Loading.args = {
-  query: {
-    isFetching: true,
-  },
-}
-
-Empty.args = {
-  query: {
-    data: [],
-  },
-}
+Default.args = {}
