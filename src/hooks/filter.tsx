651a6fc576107260f5332da4aea6a2f14fb0fcd1
nuintee <nuinteedev@gmail.com>
2023-03-12 17:11:24 +0300

    add: withAll function types/fix-all#239

--- src/hooks/filter.tsx
+++ src/hooks/filter.tsx
@@ -1,4 +1,14 @@
+import { isArray, isObject } from '@/utils/typeguards'
+
 export type ConditionsWithALL<T extends string> = T | 'ALL'
+
+export function withAll<T>(arg: T) {
+  if (isObject(arg)) {
+    return { ...arg, ALL: 'ALL' }
+  } else if (isArray(arg)) {
+    return [...arg, 'ALL']
+  }
+}
 
 export function useFilter<T>({
   array,
--- src/utils/typeguards.ts
+++ src/utils/typeguards.ts
@@ -6,7 +6,7 @@ export function isObject<T extends Object>(value: unknown): value is T {
   return true
 }
 
-export function isString(value: unknown) {
+export function isString(value: unknown): value is string {
   if (typeof value !== 'string' || value === null) {
     return false
   }
@@ -14,10 +14,14 @@ export function isString(value: unknown) {
   return true
 }
 
-export function isNumber(value: unknown) {
+export function isNumber(value: unknown): value is number {
   if (typeof value !== 'number' || value === null) {
     return false
   }
 
   return true
+}
+
+export function isArray(value: unknown): value is unknown[] {
+  return Array.isArray(value)
 }
