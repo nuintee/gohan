ef8155a4afa8b273522a4779a8d9fa22e1149947
nuintee <nuinteedev@gmail.com>
2023-03-17 19:17:31 +0300

    add: 93c45 test in details testing#205

--- src/features/details/__tests__/index.test.tsx
+++ src/features/details/__tests__/index.test.tsx
@@ -9,15 +9,41 @@ jest.mock('next/router', () => require('next-router-mock'))
 
 jest.mock('@/features/activities/hooks/useGetActivity', () =>
   // add testactivity for this specific details page to ensure it's saved
-  jest.fn().mockReturnValue({ isLoading: false, data: _testActivity }),
+  jest
+    .fn()
+    .mockReturnValueOnce({ isLoading: false, data: _testActivity })
+    .mockReturnValueOnce({ isLoading: false, data: _testActivity })
+    .mockReturnValueOnce({ isLoading: false, data: _testActivity })
+    .mockReturnValueOnce({ isLoading: false, data: _testActivity }),
 )
 
-jest.mock('@/features/details/hooks/useDetails', () =>
-  jest.fn().mockReturnValue({ isLoading: false, data: details.result('') }),
+jest.mock(
+  '@/features/details/hooks/useDetails',
+  () =>
+    jest
+      .fn()
+      .mockReturnValueOnce({
+        isLoading: false,
+        data: details.result('ChIJyfjNbFU-xxQR80zJBtL_kko'),
+      })
+      .mockReturnValueOnce({
+        isLoading: false,
+        data: details.result('ChIJyfjNbFU-xxQR80zJBtL_kko'),
+      })
+      .mockReturnValueOnce({
+        isLoading: false,
+        data: { ...details.result('ChIJBTBBRKiaqkARRgOZXBkrduI'), photos: [] },
+      }) // no photos
+      .mockReturnValueOnce({
+        isLoading: false,
+        data: details.result('ChIJBTBBRKiaqkARRgOZXBkrduI'),
+      }), // valid photos
 )
 
 describe('/details', () => {
   it('10a2c: not showing add / update review and delete review from library when unauthed', async () => {
+    // mock
+
     const page = render(<DetailsPage id={''} />, { wrapper: setUpWrapper({ isAuthed: false }) })
 
     page.debug()
@@ -36,5 +62,21 @@ describe('/details', () => {
 
     expect(activityMutationButton).toBeInTheDocument()
     expect(deleteFromLibraryDropDownItem).toBeInTheDocument()
+  })
+  it('d0060: do not show tab navigation when photos length are invalid', async () => {
+    const page = render(<DetailsPage id={''} />, { wrapper })
+
+    page.debug()
+    const tabPagination = page.queryByTestId('tab__navigation')
+
+    expect(tabPagination).not.toBeInTheDocument()
+  })
+  it('93c45: show tab navigation when photos length are morethan 1', async () => {
+    const page = render(<DetailsPage id={''} />, { wrapper })
+
+    page.debug()
+    const tabPagination = page.queryByTestId('tab__navigation')
+
+    expect(tabPagination).toBeInTheDocument()
   })
 })
