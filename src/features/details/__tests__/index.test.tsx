2487d0c3e58d771b927258c8a1544ce86542a537
nuintee <nuinteedev@gmail.com>
2023-03-17 18:27:35 +0300

    add: mocking return value with jest and next router mock testing#205

--- src/features/details/__tests__/index.test.tsx
+++ src/features/details/__tests__/index.test.tsx
@@ -1,14 +1,17 @@
 import { wrapper } from '@/config/jest/wrapper'
+import { details } from '@/data/details'
 import DetailsPage from '@/pages/details/[place_id]'
 import '@testing-library/jest-dom'
 import { render } from '@testing-library/react'
+
+jest.mock('next/router', () => require('next-router-mock'))
 
 jest.mock('@/features/activities/hooks/useGetActivity', () =>
   jest.fn().mockReturnValue({ isLoading: false }),
 )
 
 jest.mock('@/features/details/hooks/useDetails', () =>
-  jest.fn().mockReturnValue({ isLoading: false }),
+  jest.fn().mockReturnValue({ isLoading: false, data: details.result('') }),
 )
 
 describe('/details', () => {
