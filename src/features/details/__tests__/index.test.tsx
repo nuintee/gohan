52f6e6253214d0102024f2e45430d0d7572bc0eb
nuintee <nuinteedev@gmail.com>
2023-03-17 19:25:05 +0300

    add: manual mocking in each tests testing#205

--- src/features/details/__tests__/index.test.tsx
+++ src/features/details/__tests__/index.test.tsx
@@ -1,48 +1,29 @@
 import { setUpWrapper, wrapper } from '@/config/jest/wrapper'
 import { _testActivity } from '@/data/activities'
 import { details } from '@/data/details'
+import useGetActivity from '@/features/activities/hooks/useGetActivity'
 import DetailsPage from '@/pages/details/[place_id]'
 import '@testing-library/jest-dom'
 import { render } from '@testing-library/react'
+import useDetails from '../hooks/useDetails'
 
 jest.mock('next/router', () => require('next-router-mock'))
 
-jest.mock('@/features/activities/hooks/useGetActivity', () =>
-  // add testactivity for this specific details page to ensure it's saved
-  jest
-    .fn()
-    .mockReturnValueOnce({ isLoading: false, data: _testActivity })
-    .mockReturnValueOnce({ isLoading: false, data: _testActivity })
-    .mockReturnValueOnce({ isLoading: false, data: _testActivity })
-    .mockReturnValueOnce({ isLoading: false, data: _testActivity }),
-)
+jest.mock('@/features/activities/hooks/useGetActivity', () => jest.fn())
 
-jest.mock(
-  '@/features/details/hooks/useDetails',
-  () =>
-    jest
-      .fn()
-      .mockReturnValueOnce({
-        isLoading: false,
-        data: details.result('ChIJyfjNbFU-xxQR80zJBtL_kko'),
-      })
-      .mockReturnValueOnce({
-        isLoading: false,
-        data: details.result('ChIJyfjNbFU-xxQR80zJBtL_kko'),
-      })
-      .mockReturnValueOnce({
-        isLoading: false,
-        data: { ...details.result('ChIJBTBBRKiaqkARRgOZXBkrduI'), photos: [] },
-      }) // no photos
-      .mockReturnValueOnce({
-        isLoading: false,
-        data: details.result('ChIJBTBBRKiaqkARRgOZXBkrduI'),
-      }), // valid photos
-)
+jest.mock('@/features/details/hooks/useDetails', () => jest.fn())
+
+const mockGetActivity = useGetActivity as jest.Mock
+const mockGetDetails = useDetails as jest.Mock
 
 describe('/details', () => {
   it('10a2c: not showing add / update review and delete review from library when unauthed', async () => {
     // mock
+    mockGetActivity.mockReturnValue({ isLoading: false, data: _testActivity })
+    mockGetDetails.mockReturnValue({
+      isLoading: false,
+      data: details.result('ChIJyfjNbFU-xxQR80zJBtL_kko'),
+    })
 
     const page = render(<DetailsPage id={''} />, { wrapper: setUpWrapper({ isAuthed: false }) })
 
@@ -54,6 +35,12 @@ describe('/details', () => {
     expect(deleteFromLibraryDropDownItem).not.toBeInTheDocument()
   })
   it('b6a7f: showing add / update review and delete review from library when `activity` is saved', async () => {
+    mockGetActivity.mockReturnValue({ isLoading: false, data: _testActivity })
+    mockGetDetails.mockReturnValue({
+      isLoading: false,
+      data: details.result('ChIJyfjNbFU-xxQR80zJBtL_kko'),
+    })
+
     const page = render(<DetailsPage id={''} />, { wrapper })
 
     page.debug()
@@ -64,6 +51,12 @@ describe('/details', () => {
     expect(deleteFromLibraryDropDownItem).toBeInTheDocument()
   })
   it('d0060: do not show tab navigation when photos length are invalid', async () => {
+    mockGetActivity.mockReturnValue({ isLoading: false, data: _testActivity })
+    mockGetDetails.mockReturnValue({
+      isLoading: false,
+      data: { ...details.result('ChIJBTBBRKiaqkARRgOZXBkrduI'), photos: [] },
+    })
+
     const page = render(<DetailsPage id={''} />, { wrapper })
 
     page.debug()
@@ -72,6 +65,12 @@ describe('/details', () => {
     expect(tabPagination).not.toBeInTheDocument()
   })
   it('93c45: show tab navigation when photos length are morethan 1', async () => {
+    mockGetActivity.mockReturnValue({ isLoading: false, data: _testActivity })
+    mockGetDetails.mockReturnValue({
+      isLoading: false,
+      data: details.result('ChIJyfjNbFU-xxQR80zJBtL_kko'),
+    })
+
     const page = render(<DetailsPage id={''} />, { wrapper })
 
     page.debug()
