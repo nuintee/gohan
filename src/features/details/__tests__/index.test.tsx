3b957cac4e1a3d942750b6dfa78f2ca95af3e069
nuintee <nuinteedev@gmail.com>
2023-03-17 18:51:22 +0300

    add: details b6a7f testing#205

--- src/features/details/__tests__/index.test.tsx
+++ src/features/details/__tests__/index.test.tsx
@@ -8,6 +8,7 @@ import { render } from '@testing-library/react'
 jest.mock('next/router', () => require('next-router-mock'))
 
 jest.mock('@/features/activities/hooks/useGetActivity', () =>
+  // add testactivity for this specific details page to ensure it's saved
   jest.fn().mockReturnValue({ isLoading: false, data: _testActivity }),
 )
 
@@ -25,5 +26,15 @@ describe('/details', () => {
 
     expect(activityMutationButton).not.toBeInTheDocument()
     expect(deleteFromLibraryDropDownItem).not.toBeInTheDocument()
+  })
+  it('b6a7f: showing add / update review and delete review from library when `activity` is saved', async () => {
+    const page = render(<DetailsPage id={''} />, { wrapper })
+
+    page.debug()
+    const activityMutationButton = page.queryByTestId('activity_mutation__button')
+    const deleteFromLibraryDropDownItem = page.queryByText('ライブラリから削除')
+
+    expect(activityMutationButton).toBeInTheDocument()
+    expect(deleteFromLibraryDropDownItem).toBeInTheDocument()
   })
 })
