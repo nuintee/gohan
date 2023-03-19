b6557f08fa907c133476a386caf0602290ed4687
nuintee <nuinteedev@gmail.com>
2023-03-20 02:52:03 +0300

    add: ErrorText in Japanese for share function dev

--- src/utils/share.ts
+++ src/utils/share.ts
@@ -1,11 +1,13 @@
 import useToast from '@/libs/react-toastify'
 
+const ERROR_TEXT = 'お使いのブラウザは共有機能に対応しておりません。'
+
 export async function share(data: ShareData) {
   if (!navigator.canShare) {
-    useToast.error("Your browser doesn't support the Web Share API.")
+    useToast.error(ERROR_TEXT)
   } else if (navigator.canShare(data)) {
     await navigator.share(data)
   } else {
-    useToast.error("Your browser doesn't support the Web Share API.")
+    useToast.error(ERROR_TEXT)
   }
 }
