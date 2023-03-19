6cb2b09feef2ad4a92db9a7d7926530b1f55d09b
nuintee <nuinteedev@gmail.com>
2023-03-20 00:52:51 +0300

    add: dummy user data for e2e test testing#205

--- cypress/e2e/signout.cy.js
+++ cypress/e2e/signout.cy.js
@@ -2,18 +2,9 @@
 
 describe('Signout', () => {
   beforeEach(() => {
-    cy.setCookie(
-      'next-auth.session-token',
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMTIzNDU2Nzg5MCJ9.eTB-KFt1C_5yLQD28bnJrbI0KjXhtvXY1GdHhJxMKBg',
-    )
-    cy.window()
-      .its('localStorage')
-      .invoke(
-        'setItem',
-        'token',
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMTIzNDU2Nzg5MCJ9.eTB-KFt1C_5yLQD28bnJrbI0KjXhtvXY1GdHhJxMKBg',
-      )
+    cy.login()
     cy.visit('/')
+    cy.wait('@session')
   })
 
   it('it shows `Signin` when unauthed', () => {})
