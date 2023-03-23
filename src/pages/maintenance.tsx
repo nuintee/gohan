5464eb4a44dd28fefd13ecf4d7e45ed31551e259
nuintee <nuinteedev@gmail.com>
2023-03-24 02:37:23 +0300

    add: maintenance page redirect on not maintenance mode dev

--- src/pages/maintenance.tsx
+++ src/pages/maintenance.tsx
@@ -1,4 +1,5 @@
 import MaintenanceFallback from '@/components/fallback/MaintenanceFallback'
+import { IS_MAINTENANCE } from '@/config/env'
 import { MainLayout } from '@/layouts/layout'
 import { ReactElement } from 'react'
 
@@ -8,6 +9,21 @@ const MaintenancePage = () => {
 
 MaintenancePage.getLayout = function getLayout(page: ReactElement) {
   return <MainLayout disableSearch={true}>{page}</MainLayout>
+}
+
+export function getServerSideProps() {
+  if (IS_MAINTENANCE == 'false') {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: {},
+  }
 }
 
 export default MaintenancePage
