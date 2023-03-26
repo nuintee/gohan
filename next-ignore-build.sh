c80153f368c74becfb97f2567fb5de39af497a97
nuintee <nuinteedev@gmail.com>
2023-03-26 05:53:00 +0300

    add: new workflows dev

--- next-ignore-build.sh
+++ next-ignore-build.sh
@@ -2,11 +2,12 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" ]] ; then
   # Proceed with the build
     echo "✅ - Build can proceed"
   exit 1;
 
+# Include main "$VERCEL_GIT_COMMIT_REF" == "main", due to using workflow
 else
   # Don't build
   echo "🛑 - Build cancelled"
